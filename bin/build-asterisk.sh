8e56c201f15440c148466f0d9cfc85b587495ac8
Russell Bryant <russell@russellbryant.com>
2010-02-03 04:43:49 +0000

    Import initial build stuff written so far for Asterisk trunk.
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@1 a5964663-950a-4945-9369-5dd0b5cb08c1

---
+++ bin/build-asterisk.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+PLAN=$1
+BUILD_DIR=/srv/bamboo/xml-data/build-dir/${PLAN}
+
+export PATH=/usr/lib/ccache:${PATH}
+
+./configure --enable-dev-mode
+make uninstall-all
+make menuselect.makeopts
+if [ "${PLAN}" = "AST-TRUNK" ] ; then
+	sed -i -e 's/MENUSELECT_CFLAGS=/MENUSELECT_CFLAGS=TEST_FRAMEWORK /' menuselect.makeopts
+	sed -i -e 's/MENUSELECT_TEST=.*$/MENUSELECT_TEST=/' menuselect.makeopts
+fi
+make
+
+if [ "${PLAN}" != "AST-TRUNK" ] ; then
+	exit 0
+fi
+
+echo "*** Installing Asterisk and Sample Configuration ***"
+make install
+make samples
+
+echo "*** Killing off existing Asterisk processes ***"
+killall -9 asterisk
+sleep 1
+
+echo "*** Starting Asterisk ***"
+asterisk
+sleep 5
+
+echo "*** Executing Unit Tests ***"
+asterisk -rx "test execute all"
+
+echo "*** Generating Unit Test Results Output ***"
+mkdir ${BUILD_DIR}/test-reports
+asterisk -rx "test generate results xml ${BUILD_DIR}/test-reports/unit-test-results.xml"
+sleep 3
+
+echo "*** Stopping Asterisk ***"
+asterisk -rx "core stop now"
