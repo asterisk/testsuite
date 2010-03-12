c90d1532ccd364a80592ad3dcf5bad254874f112
Russell Bryant <russell@russellbryant.com>
2010-03-12 15:50:23 +0000

    Build script updates
     - use menuselect options instead of sed
     - run unit tests for any build that has them, not AST-TRUNK only hard coded
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@62 a5964663-950a-4945-9369-5dd0b5cb08c1

--- bamboo/bin/build-asterisk.sh
+++ bamboo/bin/build-asterisk.sh
@@ -6,16 +6,21 @@ TEST_RESULTS_DIR=${BUILD_DIR}/test-reports
 
 export PATH=/usr/lib/ccache:${PATH}
 
+if [ -f "main/test.c" ] ; then
+	UNIT_TESTS=yes
+else
+	UNIT_TESTS=no
+fi
+
 set -e
 
 make distclean
-
 ./configure --enable-dev-mode
 make uninstall-all
 make menuselect.makeopts
-if [ "${PLAN}" = "AST-TRUNK" ] ; then
-	sed -i -e 's/MENUSELECT_CFLAGS=/MENUSELECT_CFLAGS=TEST_FRAMEWORK /' menuselect.makeopts
-	sed -i -e 's/MENUSELECT_TEST=.*$/MENUSELECT_TEST=/' menuselect.makeopts
+if [ "${UNIT_TESTS}" = "yes" ] ; then
+	menuselect/menuselect --enable TEST_FRAMEWORK menuselect.makeopts
+	menuselect/menuselect --enable-category MENUSELECT_TEST menuselect.makeopts
 fi
 make
 
@@ -24,38 +29,40 @@ if [ -f doc/core-en_US.xml ] ; then
 	make validate-docs
 fi
 
-if [ "${PLAN}" != "AST-TRUNK" ] ; then
-	exit 0
+if [ "${UNIT_TESTS}" = "yes" ] ; then
+	echo "*** Installing Asterisk and Sample Configuration ***"
+	WGET_EXTRA_ARGS=--quiet make install
+	make samples
+
+	echo "*** Starting Asterisk ***"
+	asterisk
+	sleep 5
+
+	echo "*** Executing Unit Tests ***"
+	asterisk -rx "test execute all"
+	sleep 5
+
+	if [ ! -d ${TEST_RESULTS_DIR} ] ; then
+		mkdir ${TEST_RESULTS_DIR}
+	fi
+
+	echo "*** Generating Unit Test Results Output ***"
+	asterisk -rx "test generate results xml ${TEST_RESULTS_DIR}/unit-test-results.xml"
+	sleep 5
+
+	echo "*** Stopping Asterisk ***"
+	asterisk -rx "core stop now"
 fi
 
-echo "*** Installing Asterisk and Sample Configuration ***"
-WGET_EXTRA_ARGS=--quiet make install
-make samples
-
-echo "*** Starting Asterisk ***"
-asterisk
-sleep 5
-
-echo "*** Executing Unit Tests ***"
-asterisk -rx "test execute all"
-sleep 5
-
-if [ ! -d ${TEST_RESULTS_DIR} ] ; then
-	mkdir ${TEST_RESULTS_DIR}
+if [ "${PLAN}" = "AST-TRUNK" ] ; then
+	# Only run test suite on trunk for now, until Asterisk version handling
+	# is implemented in the test suite
+	echo "*** Running external test suite ***"
+	svn co http://svn.digium.com/svn/testsuite/asterisk/trunk testsuite
+	cd testsuite
+	./run-tests.py
+	cp *.xml ../test-reports
 fi
-
-echo "*** Generating Unit Test Results Output ***"
-asterisk -rx "test generate results xml ${TEST_RESULTS_DIR}/unit-test-results.xml"
-sleep 5
-
-echo "*** Stopping Asterisk ***"
-asterisk -rx "core stop now"
-
-echo "*** Running external test suite ***"
-svn co http://svn.digium.com/svn/testsuite/asterisk/trunk testsuite
-cd testsuite
-./run-tests.py
-cp *.xml ../test-reports
 
 echo "*** Test Results: ***"
 ls -l ${TEST_RESULTS_DIR}
