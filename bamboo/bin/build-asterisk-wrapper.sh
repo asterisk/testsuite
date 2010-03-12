61ae1965911c53c710e14755674195ff4a3911be
Russell Bryant <russell@russellbryant.com>
2010-03-12 16:29:00 +0000

    Add a build-asterisk wrapper that ensures the build script is up to date
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@63 a5964663-950a-4945-9369-5dd0b5cb08c1

---
+++ bamboo/bin/build-asterisk-wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+BINDIR=/usr/local/bin/bamboo/build-asterisk.sh
+
+svn co http://svn.digium.com/svn/testsuite/asterisk/trunk testsuite
+cd testsuite
+make install
+cd ..
+rm -rf testsuite
+
+set -e
+
+${BINDIR}/build-asterisk.sh
