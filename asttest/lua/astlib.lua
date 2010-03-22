96a46c9e7a3b1dd4b752da4db88e9b50cb6b0513
Matthew Nicholson <mnicholson@digium.com>
2010-03-22 17:18:25 +0000

    modified manager header parsing patterns to properly parse lines with mutliple colons in them
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@109 a5964663-950a-4945-9369-5dd0b5cb08c1

--- asttest/lua/astlib.lua
+++ asttest/lua/astlib.lua
@@ -409,7 +409,7 @@ function manager:_read_message()
 		return nil, err
 	end
 
-	local header, value = line:match("(.+): (.+)")
+	local header, value = line:match("([^:]+): (.+)")
 	if not header then
 		return nil, "error parsing message: " .. line
 	end
@@ -441,7 +441,7 @@ function manager:_read_message()
 
 		-- don't attempt to match headers when in data mode
 		if not data_mode then
-			header, value = line:match("(.+): ?(.*)")
+			header, value = line:match("([^:]+): ?(.*)")
 		else
 			header, value = nil, nil
 		end
