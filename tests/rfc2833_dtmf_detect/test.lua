5845c2ce65d44a6c22df391f1bac8461f96c5a02
Mark Michelson <mmichelson@digium.com>
2010-03-25 16:27:08 +0000

    Add external test for RFC2833 DTMF detection.
    From reviewboard:
    There are three test cases run. In each, SIPp is used to place a call to Asterisk, playback one or multiple pcap files and then send a BYE. The test.lua script is set to run a handler for each VarSet manager event that is received. In the handler, we compare the received DTMF digits to what we expected to receive and fail if there is a discrepancy.
    
    The pcap files that I used for playing DTMF could not be attached, so I will include a brief description of them here:
    
    dtmf_2833_*.pcap: These are the RFC2833 DTMF pcap files included in the distribution of SIPp. Strangely, they do not include a '0' DTMF digit.
    dtmf_2833_*_noend.pcap: These are generated from the original DTMF pcap files by chopping off the end frames.
    broken_dtmf.pcap: This is a series of digits that was collected from the reporter of Mantis issue 15811 who was having a problem with duplicate DTMF being detected. The quirk with the DTMF here is that there are out-of-order frames received.
    
    If more information is required regarding the tests run, see
    tests/rfc2833_dtmf_detect/test-config.yaml
    
    Review: https://reviewboard.asterisk.org/r/560
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@143 a5964663-950a-4945-9369-5dd0b5cb08c1

---
+++ tests/rfc2833_dtmf_detect/test.lua
@@ -0,0 +1,106 @@
+function sipp_exec(scenario, name, local_port)
+	return proc.exec_io("sipp",
+	"127.0.0.1",
+	"-m", "1",
+	"-sf", scenario,
+	"-i", "127.0.0.1",
+	"-p", local_port,
+	"-timeout", "30",
+	"-trace_err"
+	)
+end
+
+function sipp_exec_and_wait(scenario, name, local_port)
+	return sipp_check_error(sipp_exec(scenario, name, local_port), scenario)
+end
+
+function sipp_check_error(p, scenario)
+	local res, err = p:wait()
+
+	if not res then error(err) end
+	if res ~= 0 then
+		error("error while executing " .. scenario .. " sipp scenario (sipp exited with status " .. res .. ")\n" .. p.stderr:read("*a"))
+	end
+
+	return res, err
+end
+
+function varset_event_one(event)
+	if (event["Variable"] == "READRESULT") then
+		if (event["Value"] ~= "123456789*") then
+			fail("DTMF not detected properly. Expected '123456789*' but received " .. event["Value"])
+		end
+	end
+end
+
+function varset_event_two(event)
+	if (event["Variable"] == "READRESULT") then
+		if (event["Value"] ~= "1000") then
+			fail("DTMF not detected properly. Expected '1000' but received " .. event["Value"])
+		end
+	end
+end
+
+function varset_event_three(event)
+	if (event["Variable"] == "READRESULT") then
+		if (event["Value"] ~= "1234") then
+			fail("DTMF not detected properly. Expected '1234' but received " .. event["Value"])
+		end
+	end
+end
+
+function manager_setup(a)
+	m,err = a:manager_connect()
+	if not m then
+		fail("error connecting to asterisk: " .. err)
+	end
+
+	login = ast.manager.action:new("login")
+	login["Username"] = "asttest"
+	login["Secret"] = "asttest"
+
+	local r = m(login)
+	if not r then
+		fail("error logging in to the manager: " .. err)
+	end
+
+	if r["Response"] ~= "Success" then
+		fail("error authenticating: " .. r["Message"])
+	end
+end
+
+function test_call(scenario, handler, name, local_port)
+	m:register_event("VarSet", handler)
+	local t1 = sipp_exec(scenario, name, local_port)
+
+	--wait for everything to finish
+	sipp_check_error(t1, scenario)
+	posix.sleep(1)
+	local res, err = m:pump_messages()
+	if not res then
+		fail("error pumping manager messages: " .. err)
+	end
+	m:process_events()
+	m:unregister_event("VarSet", handler)
+end
+
+function do_dtmf_and_check_results(name)
+	local a = ast.new()
+	a:load_config("configs/sip.conf")
+	a:load_config("configs/extensions.conf")
+	a:generate_manager_conf()
+	a:spawn()
+
+	manager_setup(a)
+
+	--register our peer
+	sipp_exec_and_wait("sipp/register.xml", name, "5061")
+
+	test_call("sipp/dtmf_baseline.xml", varset_event_one, name, "5061")
+	test_call("sipp/broken_dtmf.xml", varset_event_two, name, "5061")
+	test_call("sipp/dtmf_noend.xml", varset_event_three, name, "5061")
+
+	a:term_or_kill()
+end
+
+do_dtmf_and_check_results("test1")
