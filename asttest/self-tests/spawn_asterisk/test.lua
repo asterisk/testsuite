c7daa0fa0d4e004a6a52a61beb04e26423cd8231
Matthew Nicholson <mnicholson@digium.com>
2010-06-24 14:59:36 +0000

    Make the spawn_asterisk self test spawn 10 instances of asterisk
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@426 a5964663-950a-4945-9369-5dd0b5cb08c1

--- asttest/self-tests/spawn_asterisk/test.lua
+++ asttest/self-tests/spawn_asterisk/test.lua
@@ -2,15 +2,23 @@
 
 skip_if(not ast.exists(), "asterisk not found")
 
-a = ast.new()
-a:spawn()
-res, err = proc.perror(a:term_or_kill())
-
-if res == nil then
-	fail("error running asterisk")
-elseif res ~= 0 then
-	fail("error, asterisk exited with status " .. res)
+instances = {}
+for i=1,10 do
+	print("starting asterisk instance " .. i)
+	local a = ast.new()
+	a:spawn()
+	table.insert(instances, a)
 end
 
-pass("asterisk exited with status " .. res)
+for i=1,10 do
+	print("killing asterisk instance " .. i)
+	local a = instances[i]
+	local res, err = proc.perror(a:term_or_kill())
+
+	if res == nil then
+		fail("error running asterisk")
+	elseif res ~= 0 then
+		fail("error, asterisk exited with status " .. res)
+	end
+end
 
