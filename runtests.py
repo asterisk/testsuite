3ef49c09b54a27aa932deb4672f92ff0175d8cfd
Russell Bryant <russell@russellbryant.com>
2010-08-23 15:38:28 +0000

    Print out PATH with test results for debugging purposes.
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@775 a5964663-950a-4945-9369-5dd0b5cb08c1

--- runtests.py
+++ runtests.py
@@ -416,7 +416,8 @@ def main(argv=None):
 
     test_suite.run()
 
-    print "\n=== TEST RESULTS ==="
+    print "\n=== TEST RESULTS ===\n"
+    print "PATH: %s\n" os.getenv("PATH")
     for t in test_suite.tests:
         sys.stdout.write("--> %s --- " % t.test_name)
         if t.did_run is False:
