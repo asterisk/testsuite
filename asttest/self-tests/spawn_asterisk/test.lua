809244e2d4a4d17f466e674aa4517b0431866c45
Matthew Nicholson <mnicholson@digium.com>
2010-11-02 18:09:15 +0000

    Only start and stop 5 instances of asterisk instead of 10 for spawn self test.
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@818 a5964663-950a-4945-9369-5dd0b5cb08c1

--- asttest/self-tests/spawn_asterisk/test.lua
+++ asttest/self-tests/spawn_asterisk/test.lua
@@ -2,15 +2,16 @@
 
 skip_if(not ast.exists(), "asterisk not found")
 
+count = 5
 instances = {}
-for i=1,10 do
+for i=1,count do
 	print("starting asterisk instance " .. i)
 	local a = ast.new()
 	a:spawn()
 	table.insert(instances, a)
 end
 
-for i=1,10 do
+for i=1,count do
 	print("killing asterisk instance " .. i)
 	local a = instances[i]
 	local res, err = proc.perror(a:term_or_kill())
