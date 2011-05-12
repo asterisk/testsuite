736210204ce65fdb3ca843674b670e1ecf206584
Paul Belanger <paul.belanger@polybeacon.com>
2011-05-12 16:16:28 +0000

    Create TestCase python class.
    This class now contains some common fuctions that most tests use.  I've also
    2 tests to use the new class.  Moving forward, other tests will be converted
    to use it.  Additionally, more documentation and examples will be added
    providing more information about it.
    
    Review: https://reviewboard.asterisk.org/r/1054/
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@1480 a5964663-950a-4945-9369-5dd0b5cb08c1

--- lib/python/asterisk/asterisk.py
+++ lib/python/asterisk/asterisk.py
@@ -43,24 +43,25 @@ class Asterisk:
     asterisk.conf.
     """
 
-    def __init__(self, base=None, ast_conf_options=None):
+    def __init__(self, base=None, ast_conf_options=None, host="127.0.0.1"):
         """Construct an Asterisk instance.
 
         Keyword Arguments:
         base -- This is the root of the files associated with this instance of
-        Asterisk.  By default, the base is "tmp/" within the current working
-        directory.  Given a base, a unique directory name will be generated to
-        hold all files.
+        Asterisk.  By default, the base is "/tmp/asterisk-testsuite" directory.
+        Given a base, it will be appended to the default base directory.
 
         Example Usage:
-        self.asterisk = Asterisk(base=os.path.join(os.getcwd(),
-                                                   "tests/ami-login/tmp"))
+        self.asterisk = Asterisk(base="manager/login")
         """
         self.directories = {}
         self.ast_version = AsteriskVersion()
 
+        self.base = "/tmp/asterisk-testsuite"
         self.astetcdir = "/etc/asterisk"
         self.ast_binary = utils.which("asterisk") or "/usr/sbin/asterisk"
+        self.host = host
+        self.valgrind = False
 
         # Find the system installed asterisk.conf
         ast_confs = [
@@ -76,10 +77,8 @@ class Asterisk:
             print "No asterisk.conf found on the system!"
             return
 
-        # Choose an install base
-        self.base = base
-        if self.base is None:
-            self.base = "%s/tmp" % os.getcwd()
+        if base is not None:
+            self.base = "%s/%s" % (self.base, base)
         i = 1
         while True:
             if not os.path.isdir("%s/ast%d" % (self.base, i)):
@@ -115,6 +114,8 @@ class Asterisk:
             "-f", "-g", "-q", "-m", "-n",
             "-C", "%s" % os.path.join(self.astetcdir, "asterisk.conf")
         ]
+        if self.valgrind:
+            cmd.insert(0, "valgrind")
         try:
             self.process = subprocess.Popen(cmd)
         except OSError:
