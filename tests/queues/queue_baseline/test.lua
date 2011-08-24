7191ba973197296b5260be746903d8b227974bbb
Matthew Nicholson <mnicholson@digium.com>
2011-08-24 20:00:18 +0000

    made queue_baseline more reliable and simpler
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@1911 a5964663-950a-4945-9369-5dd0b5cb08c1

--- tests/queues/queue_baseline/test.lua
+++ tests/queues/queue_baseline/test.lua
@@ -1,3 +1,5 @@
+require "watcher"
+
 function manager_setup(a)
 	local m,err = a:manager_connect()
 	if not m then
@@ -20,42 +22,22 @@ function manager_setup(a)
 	return m
 end
 
-join = nil
-function join_event(event)
-	join = 1
-end
-
-called = nil
-function called_event(event)
-	called = 1
-end
-
-connect = nil
-function connect_event(event)
-	connect = 1
-end
-
-leave = nil
-function leave_event(event)
-	leave = 1
-end
-
-complete = nil
-function complete_event(event)
-	complete = 1
-end
-
 function do_call(man)
 	local orig = ast.manager.action:new("Originate")
 	orig["Channel"] = "Local/test@test_context"
 	orig["Exten"] = "queue"
 	orig["Context"] = "test_context"
 	orig["Priority"] = "1"
-	man:register_event("Join", join_event)
-	man:register_event("AgentCalled", called_event)
-	man:register_event("AgentConnect", connect_event)
-	man:register_event("Leave", leave_event)
-	man:register_event("AgentComplete", complete_event)
+
+	local e = watcher.event.new;
+	local events = watcher.etree:new{
+		e("Join"),
+		e("AgentCalled"),
+		e("AgentConnect"),
+		e("Leave"),
+		e("AgentComplete"),
+	}
+
 	local res, err = man(orig)
 	if not res then
 		fail("Error originating call: " .. err)
@@ -63,17 +45,26 @@ function do_call(man)
 	if res["Response"] ~= "Success" then
 		fail("Response failure for Originate: " .. res["Message"])
 	end
-	posix.sleep(3)
-	res, err = man:pump_messages()
+	
+	res, err = watcher.watch(man, events, 10000)
 	if not res then
-		fail("Error pumping messages: " .. err)
+		print("Error matching events (" .. err .. ")")
+		print "Expected (not ordered):"
+		print "   Join"
+		print "   AgentCalled"
+		print "   AgentConnect"
+		print "   Leave"
+		print "   AgentComplete"
+		print "\nReceived:"
+
+		for _, e in pairs(events.multi) do
+			print("   " .. e["Event"])
+		end
+
+		print ""
+
+		fail()
 	end
-	man:process_events()
-	man:unregister_event("Join", join_event)
-	man:unregister_event("AgentCalled", called_event)
-	man:unregister_event("AgentConnect", connect_event)
-	man:unregister_event("Leave", leave_event)
-	man:unregister_event("AgentComplete", complete_event)
 end
 
 instance = ast.new()
@@ -86,26 +77,6 @@ instance:spawn()
 man = manager_setup(instance)
 
 do_call(man)
-logoff = ast.manager.action.logoff()
-man(logoff)
+man(ast.manager.action.logoff())
 instance:term_or_kill()
 
-if not join then
-	print "Failed to capture Join event"
-end
-if not called then
-	print "Failed to capture AgentCalled event"
-end
-if not connect then
-	print "Failed to capture AgentConnect event"
-end
-if not leave then
-	print "Failed to capture Leave event"
-end
-if not complete then
-	print "Failed to capture AgentComplete event"
-end
-
-if not join or not called or not connect or not leave or not complete then
-	fail("Failed to capture one of our expected events")
-end
