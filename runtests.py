b68b756faae6a0ac3999ed1f96dc5a3c33aff095
Matthew Jordan <mjordan@digium.com>
2011-09-14 21:41:52 +0000

    Missed an additional t.deps
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@2234 a5964663-950a-4945-9369-5dd0b5cb08c1

--- runtests.py
+++ runtests.py
@@ -310,7 +310,7 @@ def main(argv=None):
             sys.stdout.write("--> %s --- " % t.test_name)
             if t.did_run is False:
                 print "SKIPPED"
-                for d in t.deps:
+                for d in t.test_config.deps:
                     print "      --> Dependency: %s -- Met: %s" % (d.name,
                                  str(d.met))
                 continue
