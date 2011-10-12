b807af6badfd99000f11429465319ddccde6f3e4
Matthew Jordan <mjordan@digium.com>
2011-10-12 22:11:46 +0000

    Updates for pessimistic mode checks
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@2566 a5964663-950a-4945-9369-5dd0b5cb08c1

--- tests/pbx/pbx_lua_background/configs/ast2/extensions.lua
+++ tests/pbx/pbx_lua_background/configs/ast2/extensions.lua
@@ -9,9 +9,11 @@ extensions.test[1234] = function(c, e)
 
     -- this line should not execute because of the preceding background
     app.userevent("TestResult", "result: fail", "error: background didn't work")
+    app.hangup()
 end
 
 extensions.test[12345] = function(c, e)
     app.userevent("TestResult", "result: pass")
+    app.hangup()
 end
 
