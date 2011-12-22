db73963593c1a4bc47d7dce51f30b5426be32977
Jonathan Rose <jrose@digium.com>
2011-12-22 22:17:15 +0000

    Adding tests from Congestion logging branch and also converting all old python CDR tests to use the new test suite.
    Also hoping this commit doesn't destroy the world. Merging this was painful.
    Review: https://reviewboard.asterisk.org/r/1637/
    (issue ASTERISK-14842)
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@2960 a5964663-950a-4945-9369-5dd0b5cb08c1

---
+++ lib/python/asterisk/CDRTestCase.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+"""
+Copyright (C) 2011, Digium Inc.
+Jonathan Rose <jrose@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+"""
+
+import sys
+import logging
+from twisted.internet import reactor
+
+sys.path.append("lib/python")
+from asterisk import Asterisk
+from TestCase import TestCase
+from cdr import AsteriskCSVCDR, AsteriskCSVCDRLine
+from collections import defaultdict
+
+logger = logging.getLogger(__name__)
+
+class CDRTestCase(TestCase):
+    """
+    CDRTestCase is a subclass of the Asterisk TestCase intended to make tests involving CDR files easier.
+
+    Example Usage:
+    class TestFoo(CDRTestCase):
+        def __init__(self):
+            TestCase.__init__(self):
+            self.add_expectation("Master", AsteriskCSVCDRLine(
+                accountcode='testsuite',
+                source='',
+                destination='1',
+                dcontext='default',
+                callerid='',
+                channel='Local/1@default-.*',
+                disposition='ANSWERED',
+                amaflags='DOCUMENTATION'
+            ))
+        def main():
+            test = TestFoo()
+            test.start_asterisk()
+            reactor.run()
+            test.stop_asterisk()
+
+            return test.results()
+    """
+
+    def __init__(self):
+        """
+        Create a single Asterisk instance.
+        """
+        TestCase.__init__(self)
+        self.CDRFileExpectations = defaultdict(list)
+        self.create_asterisk()
+
+
+    def add_expectation(self, recordname, cdrline):
+        """
+        Add an expectation to the expectation list
+
+        Keyword Arguments:
+        recordname -- name of the record in which the cdr line is expected to appear
+        cdrline -- AsteriskCSVCDRLine for an expected element of the cdr
+
+        Example Usage:
+        self.add_expectation("Master", AsteriskCSVCDRLine(accountcode="",
+            source="", destination="s", dcontext="default", callerid="",
+            channel="SIP/test-00000000", disposition="CONGESTION",
+            amaflags="DOCUMENTATION"))
+        """
+        self.CDRFileExpectations[recordname].append(cdrline)
+
+    def match_cdrs(self):
+        #Automatically invoked at the end of the test, this will test the CDR files against the listed expectations.
+        self.passed = True
+
+        for key in self.CDRFileExpectations:
+            # We'll store the cdrlines for an individual cdr log file in here
+            records = []
+
+            # Build records with just the cdrlines that belong to this record
+            for i in self.CDRFileExpectations.get(key):
+                records.append(i)
+
+            cdr_expect = AsteriskCSVCDR(records=records)
+            cdr_file = AsteriskCSVCDR(fn="%s/%s/cdr-csv/%s.csv" % (self.ast[0].base, self.ast[0].directories['astlogdir'], key))
+
+            if cdr_expect.match(cdr_file):
+                logger.debug("%s.csv - CDR results met expectations" % key)
+            else:
+                logger.error("%s.csv - CDR results did not meet expectations.  Test Failed." % key)
+                self.passed = False
+
+    def results(self):
+        """
+        Returns the test results.
+
+        Example Usage:
+        def foo(self):
+            test = bar()
+            test.start_asterisk()
+            reactor.run()
+            test.stop_asterisk()
+            return test.results()
+        """
+
+        self.match_cdrs()
+        if self.passed == True:
+            return 0
+        return 1
+
+    def ami_logoff(self, ami):
+        """
+        An AMI callback event.
+
+        """
+        self.stop_reactor()
+
+    def ami_connect(self, ami):
+        """
+        An AMI callback event from create_ami_factory() function.
+        """
+        TestCase.ami_connect(self, ami)
+        self.ami[0].originate(
+            channel = 'Local/1@default',
+            application = 'Echo'
+        ).addErrback(self.ami_logoff)
+
+    def run(self):
+        """
+        Create an AMI factory with a calback for the ami_connect() function.
+        """
+        TestCase.run(self)
+        self.create_ami_factory()
