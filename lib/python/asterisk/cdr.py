fb55398fb0aa574786f30409802510fa63912144
Walter Doekes <walter+asterisk@wjd.nu>
2012-04-19 22:15:00 +0000

    Add regression test for issue ASTERISK-19384.
    Review: https://reviewboard.asterisk.org/r/1765/
    Reviewed by: Terry Wilson
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@3201 a5964663-950a-4945-9369-5dd0b5cb08c1

--- lib/python/asterisk/cdr.py
+++ lib/python/asterisk/cdr.py
@@ -39,7 +39,7 @@ class AsteriskCSVCDRLine:
         self.__dict__.update(locals())
         del self.__dict__['self']
 
-    def match(self, other):
+    def match(self, other, silent=False):
         """Matches if the subset of fields that exist in both records match.
 
         It is important to make sure that if you want to test against an empty
@@ -47,11 +47,12 @@ class AsteriskCSVCDRLine:
         initialization or passed as None.
         """
 
-        for k,v in self.iteritems():
+        for k, v in self.iteritems():
             if None not in (v, other.get(k)) and not re.match(
                     "%s$" % (str(v).lower()), str(other.get(k)).lower()):
-                logger.warn("CDR MATCH FAILED, Expected: %s:%s Got: %s:%s" % (k, v,
-                        k, other.get(k)))
+                if not silent:
+                    logger.warn("CDR MATCH FAILED, Expected: %s:%s Got: %s:%s" %
+                            (k, v, k, other.get(k)))
                 return False
         return True
 
@@ -120,9 +121,63 @@ class AsteriskCSVCDR:
         if len(self) != len(other):
             logger.warn("CDR MATCH FAILED, different number of records")
             return False
-        for i,x in enumerate(self):
-            if not x.match(other[i]):
-                return False
+
+        def match_order(list_a, list_b, cmp_func):
+            """Utility function that attempts to find out whether list_a and
+            list_b have a single ordering match or if there is more than
+            one.
+
+            >>> f = (lambda x, y: x == y)
+            >>> match_order(['A', 'B', 'C'], ['C', 'B', 'X'], f)
+            ()
+            >>> match_order(['A', 'B', 'C'], ['C', 'B', 'A'], f)
+            ((2, 1, 0),)
+            >>> match_order(['A', 'B', 'C', 'B'], ['C', 'B', 'A', 'B'], f)
+            ((2, 1, 0, 3), (2, 3, 0, 1))
+            """
+            assert len(list_a) == len(list_b)
+            size = len(list_a)
+
+            # attempt two orderings: forward and reversed
+            guess_orders = (range(size), list(reversed(range(size)))) # both mutable
+            found_orders = []
+
+            for guess_order in guess_orders:
+                found_order = []
+                for a in range(size):
+                    for b in guess_order:
+                        if cmp_func(list_a[a], list_b[b]):
+                            found_order.append(b)
+                            guess_order.remove(b)
+                            break
+                    else:
+                        # no match at all..
+                        return ()
+                found_orders.append(tuple(found_order))
+
+            if found_orders[0] != found_orders[1]:
+                return tuple(found_orders)
+            return (found_orders[0],)
+
+        # Use the match_order function to see if there either is (a) no match,
+        # or (b) a single match or (c) several matches. In the latter case, the
+        # regexes should probably be chosen more carefully.
+        matches = match_order(self, other, (lambda x, y: x.match(y, silent=True)))
+
+        if len(matches) == 0:
+            # Bah.. no match. Loop over the records in the normal order and
+            # have it complain immediately.
+            for i, x in enumerate(self):
+                if not x.match(other[i]):
+                    return False
+            assert False
+
+        elif len(matches) == 1:
+            pass # joy!
+
+        elif len(matches) > 1:
+            logger.warn("More than one CDR permutation results in success")
+
         return True
 
     def __str__(self):
