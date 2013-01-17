a42d1811d311a8fa4836218a0d2d46b3733da0de
Kinsey Moore <kmoore@digium.com>
2013-01-17 21:30:31 +0000

    Add AMI event triggering of originations
    This adds the ability to trigger originations from any AMI event using
    YAML configuration as an extension of the Originator pluggable module.
    As a part of this enhancement, AMIPrivateCallbackInstance was added
    which can be used to hook AMI events with an arbitrary callback if a
    YAML definition of the event is already available in the format used
    by AMIEventModule.
    
    Review: https://reviewboard.asterisk.org/r/2261/
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@3606 a5964663-950a-4945-9369-5dd0b5cb08c1

--- lib/python/asterisk/PluggableModules.py
+++ lib/python/asterisk/PluggableModules.py
@@ -11,6 +11,7 @@ import sys
 import logging
 
 sys.path.append("lib/python")
+from ami import AMIEventInstance
 
 LOGGER = logging.getLogger(__name__)
 
@@ -22,6 +23,7 @@ class Originator(object):
         test_object.register_scenario_started_observer(self.scenario_started)
         self.test_object = test_object
         self.current_destination = 0
+        self.ami_callback = None
         self.config = {
             'channel': 'Local/s@default',
             'application': 'Echo',
@@ -31,7 +33,8 @@ class Originator(object):
             'priority': '',
             'ignore-originate-failure': 'no',
             'trigger': 'scenario_start',
-            'id': '0'
+            'id': '0',
+            'event': None
         }
 
         # process config
@@ -40,6 +43,18 @@ class Originator(object):
         for k in module_config.keys():
             if k in self.config:
                 self.config[k] = module_config[k]
+
+        # create event callback if necessary
+        if self.config['trigger'] == 'event':
+            if not self.config['event']:
+                LOGGER.error("Event specifier for trigger type 'event' is missing")
+                raise Exception
+
+            # set id to the AMI id for the origination if it is unset
+            if 'id' not in self.config['event']:
+                self.config['event']['id'] = self.config['id']
+
+            self.ami_callback = AMIPrivateCallbackInstance(self.config['event'], test_object, self.originate_callback)
         return
 
     def ami_connect(self, ami):
@@ -59,6 +74,12 @@ class Originator(object):
             self.test_object.set_passed(False)
         return None
 
+    def originate_callback(self, ami, event):
+        '''Handle event callbacks.'''
+        LOGGER.info('Got event callback')
+        self.originate_call()
+        return True
+
     def originate_call(self):
         '''Handle origination of the call based on the options provided in the configuration.'''
         LOGGER.info("Originating call")
@@ -77,3 +98,22 @@ class Originator(object):
         if self.config['trigger'] == 'scenario_start':
             self.originate_call()
         return result
+
+class AMIPrivateCallbackInstance(AMIEventInstance):
+    '''
+    Subclass of AMIEventInstance that operates by calling a user-defined
+    callback function. The callback function returns the current disposition
+    of the test (i.e. whether the test is currently passing or failing).
+    '''
+    def __init__(self, instance_config, test_object, callback):
+        super(AMIPrivateCallbackInstance, self).__init__(instance_config, test_object)
+        self.callback = callback
+        if 'start' in instance_config:
+            self.passed = True if instance_config['start'] == 'pass' else False
+
+    def event_callback(self, ami, event):
+        self.passed = self.callback(ami, event)
+
+    def check_result(self, callback_param):
+        self.test_object.set_passed(self.passed)
+        return callback_param
