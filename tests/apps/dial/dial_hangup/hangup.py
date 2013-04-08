4c7ffe25b973db5c8c85bdc9ac300e5c13ba8f18
Matthew Jordan <mjordan@digium.com>
2013-04-08 15:48:19 +0000

    Add basic dial tests
    This adds tests for basic dialing in Asterisk. This checks a variety of two-party
    dialing scenarios:
     * Dialing a single party with nominal/off-nominal results
     * Dialing multiple parties in parallel with nominal/off-nominal results
     * Dialing where the caller hangs up before the remote party answers
    
    For all tests, AMI events are checked.
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@3687 a5964663-950a-4945-9369-5dd0b5cb08c1

---
+++ tests/apps/dial/dial_hangup/hangup.py
@@ -0,0 +1,20 @@
+'''
+Copyright (C) 2013, Digium, Inc.
+Matt Jordan <mjordan@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+'''
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+def send_hangup(ami, event):
+    ''' Callback called when we detect dial has started.
+    '''
+
+    channel = event['channel']
+    LOGGER.info('Hanging up channel %s' % channel)
+    ami.hangup(channel)
+
---
+++ tests/apps/dial/dial_parallel/dial_parallel_all_cancel/send_dtmf.py
@@ -0,0 +1,21 @@
+'''
+Copyright (C) 2013, Digium, Inc.
+Matt Jordan <mjordan@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+'''
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+def send_dtmf(ami, event):
+    ''' Callback called when we detect dial has started.
+    '''
+
+    channel = event['channel'][:len(event['channel']) - 2]
+    channel += ';1'
+    LOGGER.info('Sending DTMF to hangup channel %s' % channel)
+    ami.redirect(channel, 'default', 'dtmf', '1')
+
