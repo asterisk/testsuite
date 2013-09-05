2b855bf89d55a49f3e6da303b0fb270ae507de31
Jonathan Rose <jrose@digium.com>
2013-09-05 16:18:28 +0000

    testsuite: Update call parking timeout tests to accomodate Asterisk 12
    (issue ASTERISK-22328)
    Reported by: Matt Jordan
    Review: https://reviewboard.asterisk.org/r/2816/
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@4111 a5964663-950a-4945-9369-5dd0b5cb08c1

--- tests/bridge/parkcall_timeout/comebacktoorigin_no/Executioner.py
+++ tests/bridge/parkcall_timeout/comebacktoorigin_no/Executioner.py
@@ -9,11 +9,13 @@ the GNU General Public License Version 2.
 
 import sys
 import logging
-import re
 
 sys.path.append("lib/python")
 
+from version import AsteriskVersion
+
 LOGGER = logging.getLogger(__name__)
+
 
 class Executioner(object):
     def __init__(self, module_config, test_object):
@@ -22,14 +24,25 @@ class Executioner(object):
         test_object.register_ami_observer(self.ami_connect)
         self.test_object = test_object
 
+        running_version = AsteriskVersion()
+        if (running_version < AsteriskVersion("12.0.0")):
+            self.asterisk12Events = False
+        else:
+            self.asterisk12Events = True
+
         self.calls = []
-        self.calls.append({'test' : '1', 'parker' : 'SIP/alice', 'lot' : 'parkinglot_test1', 'slot' : '401'})
-        self.calls.append({'test' : '2', 'parker' : 'SIP/alice', 'lot' : 'parkinglot_test2', 'slot' : '501'})
-        self.calls.append({'test' : '3', 'parker' : 'SIP/alice', 'lot' : 'parkinglot_test3', 'slot' : '601'})
+        self.calls.append({'test': '1', 'parker': 'SIP/alice',
+                          'lot': 'parkinglot_test1', 'slot': '401'})
+        self.calls.append({'test': '2', 'parker': 'SIP/alice',
+                          'lot': 'parkinglot_test2', 'slot': '501'})
+        self.calls.append({'test': '3', 'parker': 'SIP/alice',
+                           'lot': 'parkinglot_test3', 'slot': '601'})
         self.userevents_received = 0
         self.passed_dialplan = 0
         self.failures_logged = 0
-        self.fail_token = self.test_object.create_fail_token("No success indicated by Executioner.")
+        self.fail_token = \
+            self.test_object.create_fail_token("No success indicated by "
+                                               "Executioner.")
         return
 
     def ami_connect(self, ami):
@@ -47,14 +60,29 @@ class Executioner(object):
             not_right = True
         if event.get('application') != 'Dial':
             not_right = True
-        if event.get('appdata') != 'SIP/alice,3,Hk':
-            not_right = True
-        if event.get('registrar') != 'features':
-            not_right = True
+
+        appdata = event.get('appdata')
+        registrar = event.get('registrar')
+
+        if self.asterisk12Events:
+            # Asterisk 12's parking uses a different registrar from previous
+            # versions and puts features on channels directly instead of in
+            # the dialplan
+            if appdata != 'SIP/alice,3':
+                not_right = True
+            if registrar != 'res_parking':
+                not_right = True
+        else:
+            if appdata != 'SIP/alice,3,Hk':
+                not_right = True
+            if registrar != 'features':
+                not_right = True
 
         if not_right:
-            # We don't handle failure here since the last check_user_event will simply see if one ever succeeded
-            LOGGER.info("Received a dialplan entry that didn't match the expected one.")
+            # We don't handle failure here since the last check_user_event
+            # will simply see if one ever succeeded
+            LOGGER.info("Received a dialplan entry that didn't match the "
+                        "expected one.")
             return
 
         LOGGER.info("Received a dialplan entry that matched our expectations.")
@@ -82,50 +110,70 @@ class Executioner(object):
 
         # Make sure the test wasn't from a failure condition
         if not event.get('success'):
-            LOGGER.error("Test Phase %d: User Event didn't include a success tag. Test Failed." % this_test)
+            LOGGER.error("Test Phase %d: User Event didn't include a success "
+                         "tag. Test Failed." % this_test)
             self.failures_logged += 1
 
         if event.get('success') != 'true':
-            LOGGER.error("Test Phase %d: User Event didn't indicate success. Test Failed." % this_test)
+            LOGGER.error("Test Phase %d: User Event didn't indicate success. "
+                         "Test Failed." % this_test)
             self.failures_logged += 1
 
-        # Make sure each variable that was supposed to be set matches our expectations.
+        # Make sure each variable that was supposed to be set matches our
+        # expectations.
         mismatches = 0
         if event.get('parker') != this_expectation['parker']:
-            LOGGER.error("Test Phase %d: User event condition mismatch on parker. Got '%s' but '%s' was expected." % (this_test, event.get('parker'), this_expectation['parker']))
+            LOGGER.error("Test Phase %d: User event condition mismatch on "
+                         "parker. Got '%s' but '%s' was expected." %
+                         (this_test, event.get('parker'),
+                          this_expectation['parker']))
             mismatches += 1
         if event.get('slot') != this_expectation['slot']:
-            LOGGER.error("Test Phase %d: User Event condition mismatch on slot. Got '%s' but '%s' was expected." % (this_test, event.get('slot'), this_expectation['slot']))
+            LOGGER.error("Test Phase %d: User Event condition mismatch on "
+                         "slot. Got '%s' but '%s' was expected." %
+                         (this_test, event.get('slot'),
+                          this_expectation['slot']))
             mismatches += 1
         if event.get('lot') != this_expectation['lot']:
-            LOGGER.error("Test Phase %d: User Event condition mismatch on lot. Got '%s' but '%s' was expected." % (this_test, event.get('lot'), this_expectation['lot']))
+            LOGGER.error("Test Phase %d: User Event condition mismatch on "
+                         "lot. Got '%s' but '%s' was expected." %
+                         (this_test, event.get('lot'),
+                          this_expectation['lot']))
             mismatches += 1
 
         if mismatches > 0:
-            LOGGER.error("Test Phase %d: Mismatches were present in the channel variables set by park call timeout. Test failed." % this_test)
+            LOGGER.error("Test Phase %d: Mismatches were present in the "
+                         "channel variables set by park call timeout." %
+                         this_test)
             self.failures_logged += 1
 
-        # For the first test, we should also make sure the park-dial extension was added. This will require another event.
+        # For the first test, we should also make sure the park-dial extension
+        # was added. This will require another event.
         if self.userevents_received == 1:
-            message = {'action': 'ShowDialPlan', 'context': 'park-dial', 'extension': 'SIP_alice'}
+            message = {'action': 'ShowDialPlan', 'context': 'park-dial',
+                       'extension': 'SIP_alice'}
             self.ami.sendMessage(message)
 
         # Looks like the test was successful.  Yay.
         LOGGER.info("Test Phase %d: Passed." % this_test)
 
-        # Once all the tests are complete, we need to check final pass conditions
+        # Once all the tests are complete, check final pass conditions
         if len(self.calls) == 0:
-            # clear the fail token since we have reached where we decide ultimately whether it failed or not.
+            # clear the fail token since we reach the point when we can
+            # decisively find out whether it failed or not.
             self.test_object.remove_fail_token(self.fail_token)
 
             if not self.passed_dialplan:
-                LOGGER.error("We never received a ListDialPlan event with the right extension data in it. Test failed.")
+                LOGGER.error("We never received a ListDialPlan event with "
+                             "the right extension data in it. Test failed.")
                 self.failures_logged += 1
                 self.test_object.set_passed(False)
 
             if self.failures_logged == 0:
-                LOGGER.info("All phases complete and the dialplan check showed the proper entry. Yay. Test Passed.")
+                LOGGER.info("All phases complete and the dialplan check "
+                            "showed the proper entry. Yay. Test Passed.")
                 self.test_object.set_passed(True)
             else:
-                LOGGER.error("Test failed with %d errors.\n", self.failures_logged)
+                LOGGER.error("Test failed with %d errors.\n" %
+                             self.failures_logged)
                 self.test_object.set_passed(False)
--- tests/bridge/parkcall_timeout/comebacktoorigin_yes/Executioner.py
+++ tests/bridge/parkcall_timeout/comebacktoorigin_yes/Executioner.py
@@ -14,6 +14,9 @@ sys.path.append("lib/python")
 
 LOGGER = logging.getLogger(__name__)
 
+from version import AsteriskVersion
+
+
 class Executioner(object):
     def __init__(self, module_config, test_object):
         self.ami = None
@@ -22,15 +25,26 @@ class Executioner(object):
         test_object.register_ami_observer(self.ami_connect)
         self.test_object = test_object
 
+        running_version = AsteriskVersion()
+        if (running_version < AsteriskVersion("12.0.0")):
+            self.asterisk12Events = False
+        else:
+            self.asterisk12Events = True
+
         self.calls = []
-        self.calls.append({'parker' : 'SIP/alice', 'lot' : 'parkinglot_test1', 'slot' : '401', 'status' : 'ANSWER', 'post' : False})
-        self.calls.append({'parker' : 'SIP/alice', 'lot' : 'parkinglot_test1', 'slot' : '402', 'status' : 'NOANSWER', 'post' : True})
-        self.calls.append({'parker' : 'SIP/alice', 'lot' : 'parkinglot_test1', 'slot' : '403', 'status' : 'BUSY', 'post' : True})
+        self.calls.append({'parker': 'SIP/alice', 'lot': 'parkinglot_test1',
+                          'slot': '401', 'status': 'ANSWER', 'post': False})
+        self.calls.append({'parker': 'SIP/alice', 'lot': 'parkinglot_test1',
+                          'slot': '402', 'status': 'NOANSWER', 'post': True})
+        self.calls.append({'parker': 'SIP/alice', 'lot': 'parkinglot_test1',
+                          'slot': '403', 'status': 'BUSY', 'post': True})
         self.current_call = None
         self.current_call_post = False
 
         # Automatically fail if we don't remove this token.
-        self.fail_token = self.test_object.create_fail_token("This test should fail all the time right now.")
+        self.fail_token = \
+            self.test_object.create_fail_token("This test should fail all the "
+                                               "time right now.")
 
         self.userevents_received = 0
         self.failures_logged = 0
@@ -52,7 +66,8 @@ class Executioner(object):
     def respond_to_park(self, ami, event):
         self.parks_received += 1
         new_db_value = self.parks_received
-        message = {'action': 'DBPut', 'Family': 'test', 'Key': 'position', 'Val': new_db_value}
+        message = {'action': 'DBPut', 'Family': 'test', 'Key': 'position',
+                   'Val': new_db_value}
         self.alice_ami.sendMessage(message)
         self.current_call = self.calls.pop(0)
 
@@ -61,14 +76,19 @@ class Executioner(object):
 
     def user_event_match(self, event):
         num_failures = 0
+
         if (event.get('parker') != self.current_call['parker']):
-            num_failures += 1
-        if (event.get('slot') != self.current_call['slot']):
-            num_failures += 1
-        if (event.get('lot') != self.current_call['lot']):
             num_failures += 1
         if (event.get('status') != self.current_call['status']):
             num_failures += 1
+
+        if self.asterisk12Events:
+            # These values aren't set in Asterisk 11 when
+            # comebacktoorigin = yes, but they are in 12.
+            if (event.get('slot') != self.current_call['slot']):
+                num_failures += 1
+            if (event.get('lot') != self.current_call['lot']):
+                num_failures += 1
 
         if (num_failures):
             LOGGER.info("Failing event: %s" % event)
@@ -79,31 +99,43 @@ class Executioner(object):
     def check_parkhangup(self, ami, event):
         match_failures = self.user_event_match(event)
         if (match_failures):
-            LOGGER.error("Test Phase %d: %d Mismatches were observed in a park_postcall event. Test failed." % (self.parks_received, match_failures))
+            LOGGER.error("Test Phase %d: %d Mismatches were observed in a "
+                         "park_postcall event. Test failed." %
+                         (self.parks_received, match_failures))
             self.failures_logged += 1
 
-        # Check if a call received a post test user event against whether or not it was supposed to.
-        if (self.current_call['post'] == True and not self.current_call_post):
-            LOGGER.error("Test Phase %d: Test failed because this phase should have received a post call user event and didn't." % self.parks_received)
+        # Check if a call received a post test user event against whether or
+        # not it was supposed to.
+        if (self.current_call['post'] is True and not self.current_call_post):
+            LOGGER.error("Test Phase %d: Test failed because this phase "
+                         "should have received a post call user event and "
+                         "didn't." % self.parks_received)
             self.failures_logged += 1
 
-        if (self.current_call['post'] == False and self.current_call_post):
-            LOGGER.error("Test Phase %d: Test failed because this phase should not have received a post call user event and did." % self.parks_received)
+        if (self.current_call['post'] is False and self.current_call_post):
+            LOGGER.error("Test Phase %d: Test failed because this phase "
+                         "should not have received a post call user event "
+                         "and did." % self.parks_received)
             self.failures_logged += 1
 
-        # Once all the tests are complete, we need to check final pass conditions
+        # Once all the tests are complete, check final pass conditions
         if len(self.calls) == 0:
             self.test_object.remove_fail_token(self.fail_token)
             if self.failures_logged == 0:
-                LOGGER.info("All phases complete and the dialplan check showed the proper entry. Yay. Test Passed.")
+                LOGGER.info("All phases complete and the dialplan check "
+                            "showed the proper entry. Yay. Test Passed.")
                 self.test_object.set_passed(True)
             else:
-                LOGGER.error("Test failed with %d errors.\n", self.failures_logged)
+                LOGGER.error("Test failed with %d errors.\n" %
+                             self.failures_logged)
+                self.test_object.set_passed(False)
 
     def check_parkpostcall(self, ami, event):
         match_failures = self.user_event_match(event)
         if (match_failures):
-            LOGGER.error("Test Phase %d: %d mismatches were observed in a park_postcall event. Test failed." % (self.parks_received, match_failures))
+            LOGGER.error("Test Phase %d: %d mismatches were observed in a "
+                         "park_postcall event. Test failed." %
+                         (self.parks_received, match_failures))
             self.failures_logged += 1
 
         self.current_call_post = True
