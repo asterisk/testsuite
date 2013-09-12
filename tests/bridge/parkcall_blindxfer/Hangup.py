eddab36e486773053714af8f72e711c8095893a1
Jonathan Rose <jrose@digium.com>
2013-09-12 17:46:39 +0000

    Testsuite: Update bridge/parkcall_blindxfer test for res_parking
    (closes issue ASTERISK-22328)
    Reported by: Matt Jordan
    Review: https://reviewboard.asterisk.org/r/2815/
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@4169 a5964663-950a-4945-9369-5dd0b5cb08c1

--- tests/bridge/parkcall_blindxfer/Hangup.py
+++ tests/bridge/parkcall_blindxfer/Hangup.py
@@ -12,20 +12,43 @@ import logging
 
 sys.path.append("lib/python")
 
+from version import AsteriskVersion
+
 LOGGER = logging.getLogger(__name__)
 
-def hangup_chan(ami, event):
-    if ami.id != 0:
+
+def handle_parkedcall(ami, event):
+    running_version = AsteriskVersion()
+
+    if running_version >= AsteriskVersion("12.0.0"):
+        parkee = event.get('parkeechannel')
+    else:
+        parkee = event.get('channel')
+
+    if parkee is None:
+        LOGGER.error("Receved ParkedCall event without a Parkee.\n")
         return False
 
-    if event.get('channel') == 'SIP/alice-00000004':
+    LOGGER.info("Hanging up channel: %s" % parkee)
+    ami.hangup(parkee)
+
+    return True
+
+
+def handle_testevent(ami, event):
+    parkee = event.get('channel')
+
+    if parkee is None:
+        LOGGER.error("Received TestEvent without a channel.\n")
+
+    if parkee == 'SIP/alice-00000004':
         # channel which parking failed for
         LOGGER.info("Hanging up channel: SIP/bob-00000005")
         ami.hangup('SIP/bob-00000005')
         # the call that was parked
         LOGGER.info("Hanging up channel: Local/fill-park@default-00000000;2")
         ami.hangup('Local/fill-park@default-00000000;2')
-    elif event.get('channel') == 'SIP/alice-00000008':
+    elif parkee == 'SIP/alice-00000008':
         # channel which parking failed for
         LOGGER.info("Hanging up channel: SIP/bob-00000009")
         ami.hangup('SIP/bob-00000009')
@@ -33,9 +56,8 @@ def hangup_chan(ami, event):
         LOGGER.info("Hanging up channel: Local/fill-park@default-00000001;2")
         ami.hangup('Local/fill-park@default-00000001;2')
     else:
-        LOGGER.info("Hanging up channel: %s" % event.get('channel'))
-        ami.hangup(event.get('channel'))
+        LOGGER.info("Hanging up channel: %s" % parkee)
+        ami.hangup(parkee)
 
     return True
-
 # vim:sw=4:ts=4:expandtab:textwidth=79
