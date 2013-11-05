7f4219778efa1314543aa2014aa1d01267bcebae
Joshua Colp <jcolp@digium.com>
2013-11-05 12:27:15 +0000

    Add a test covering origination using ARI.
    (issue ASTERISK-22768)
    Reported by: Matt Jordan
    
    Review: https://reviewboard.asterisk.org/r/2980/
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@4326 a5964663-950a-4945-9369-5dd0b5cb08c1

---
+++ tests/rest_api/channels/originate/channel_originate.py
@@ -0,0 +1,23 @@
+'''
+Copyright (C) 2013, Digium, Inc.
+Joshua Colp <jcolp@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+'''
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+def on_kickoff_start(ari, event):
+    LOGGER.debug("on_kickoff_start(%r)" % event)
+    for x in xrange(50):
+        ari.post('channels', endpoint='Local/1000@default', app='testsuite', appArgs='blast')
+    ari.delete('channels', event['channel']['id'])
+    return True
+
+def on_blast_start(ari, event):
+    LOGGER.debug("on_blast_start(%r)" % event)
+    ari.delete('channels', event['channel']['id'])
+    return True
