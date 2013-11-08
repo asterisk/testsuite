e439b8f60b88d721c2eb60b92dc5faa7c6b0e418
Joshua Colp <jcolp@digium.com>
2013-11-08 11:34:26 +0000

    Fix test. The number of arguments passed in has changed.
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@4329 a5964663-950a-4945-9369-5dd0b5cb08c1

--- tests/rest_api/channels/originate/channel_originate.py
+++ tests/rest_api/channels/originate/channel_originate.py
@@ -10,14 +10,14 @@ import logging
 
 LOGGER = logging.getLogger(__name__)
 
-def on_kickoff_start(ari, event):
+def on_kickoff_start(ari, event, test_object):
     LOGGER.debug("on_kickoff_start(%r)" % event)
     for x in xrange(50):
         ari.post('channels', endpoint='Local/1000@default', app='testsuite', appArgs='blast')
     ari.delete('channels', event['channel']['id'])
     return True
 
-def on_blast_start(ari, event):
+def on_blast_start(ari, event, test_object):
     LOGGER.debug("on_blast_start(%r)" % event)
     ari.delete('channels', event['channel']['id'])
     return True
