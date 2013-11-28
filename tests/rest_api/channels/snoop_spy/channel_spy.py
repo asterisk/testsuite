b329da72678e478248c41f5e071fb558062c8910
Joshua Colp <jcolp@digium.com>
2013-11-28 00:48:17 +0000

    Add tests which cover Snoop spying and whispering in ARI.
    (closes issue ASTERISK-22780)
    Reported by: Matt Jordan
    
    Review: https://reviewboard.asterisk.org/r/3007/
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@4383 a5964663-950a-4945-9369-5dd0b5cb08c1

---
+++ tests/rest_api/channels/snoop_spy/channel_spy.py
@@ -0,0 +1,35 @@
+'''
+Copyright (C) 2013, Digium, Inc.
+Joshua Colp <jcolp@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+'''
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+class Snoop(object):
+    def __init__(self):
+        self.bridge_id = None
+
+TEST = Snoop()
+
+def on_start(ari, event, test_object):
+    LOGGER.debug("on_start(%r)" % event)
+    ari.post('channels', event['channel']['id'], 'snoop', spy='in', whisper='none', app='testsuite', appArgs='snoop')
+    ari.post('channels', event['channel']['id'], 'play', media='sound:demo-congrats')
+    return True
+
+def on_snoop_start(ari, event, test_object):
+    LOGGER.debug("on_snoop_start(%r)" % event)
+    TEST.bridge_id = ari.post('bridges').json()['id']
+    ari.post('bridges', TEST.bridge_id, 'addChannel', channel=event['channel']['id'])
+    ari.post('channels', endpoint='Local/amd@default', app='testsuite', appArgs='amd')
+    return True
+
+def on_amd_start(ari, event, test_object):
+    LOGGER.debug("on_amd_start(%r)" % event)
+    ari.post('bridges', TEST.bridge_id, 'addChannel', channel=event['channel']['id'])
+    return True
---
+++ tests/rest_api/channels/snoop_whisper/channel_whisper.py
@@ -0,0 +1,22 @@
+'''
+Copyright (C) 2013, Digium, Inc.
+Joshua Colp <jcolp@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+'''
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+def on_start(ari, event, test_object):
+    LOGGER.debug("on_start(%r)" % event)
+    ari.post('channels', event['channel']['id'], 'snoop', spy='none', whisper='out', app='testsuite', appArgs='snoop')
+    ari.post('channels', event['channel']['id'], 'play', media='sound:silence/10')
+    return True
+
+def on_snoop_start(ari, event, test_object):
+    LOGGER.debug("on_snoop_start(%r)" % event)
+    ari.post('channels', event['channel']['id'], 'play', media='sound:demo-congrats')
+    return True
