541fb08c4c6d54a2dc29b1c50e8621b48e34615e
Matthew Jordan <mjordan@digium.com>
2013-12-09 04:37:40 +0000

    show_subscriptions: Update test to not terminate on SIPp Scenario
    The test object for the show_subscriptions test was inheriting from
    the SIPpTestCase class. This class can only be used for tests that terminate
    when the SIPp scenarios terminate, as the class will automatically stop the
    reactor when the last SIPp scenario finishes. In the case of this test, the
    SIPp scenario is merely used to create a subscription so that a subsequent
    AMI action will successfully query for the subscriptions. Since the
    scenario stops the reactor and kills Asterisk, this was unlikely to succeed
    unless the AMI action ran at exactly the right time.
    
    This patch changes the test object so that it inherits from TestCase. The
    test object now manages the SIPp Scenario itself.
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@4408 a5964663-950a-4945-9369-5dd0b5cb08c1

--- tests/channels/pjsip/ami/show_subscriptions/AMISendTest.py
+++ tests/channels/pjsip/ami/show_subscriptions/AMISendTest.py
@@ -8,25 +8,40 @@ the GNU General Public License Version 2.
 '''
 
 import sys
-import time
+import logging
 
 sys.path.append("lib/python/asterisk")
 
-from sipp import SIPpTestCase
+from sipp import SIPpScenario
+from TestCase import TestCase
+
+from twisted.internet import reactor
+
+logger = logging.getLogger(__name__)
 
 ACTION = {
     "Action":"PJSIPShowSubscriptionsInbound"
 }
 
-class AMISendTest(SIPpTestCase):
+class AMISendTest(TestCase):
     def __init__(self, path=None, config=None):
         super(AMISendTest, self).__init__(path, config)
+        self.create_asterisk()
 
     def run(self):
         super(AMISendTest, self).run()
+        self.create_ami_factory()
 
     def ami_connect(self, ami):
         super(AMISendTest, self).ami_connect(ami)
-        # give some time for everything to subscribe
-        time.sleep(3)
-        ami.sendDeferred(ACTION).addCallback(ami.errorUnlessResponse)
+
+        def _send_show_subscriptions(obj):
+            logger.info('Getting inbound subscriptions...')
+            ami.sendDeferred(ACTION).addCallback(ami.errorUnlessResponse)
+            reactor.callLater(2, self.stop_reactor)
+            return obj
+
+        logger.info('Starting subscription scenario')
+        sipp = SIPpScenario(self.test_name,
+            {'scenario':'subscribe.xml', '-p':'5061' })
+        sipp.run(self).addCallback(_send_show_subscriptions)
