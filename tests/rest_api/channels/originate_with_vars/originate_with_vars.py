8857d055213bd528fa11eb7bf603fdcfd70be7da
Kevin Harwell <kharwell@digium.com>
2013-12-13 17:23:54 +0000

    ARI: Allow specifying channel variables during a POST /channels
    Test for originating a call with channel variables. Originates a call using ARI
    and checks to make sure the specified channel variable(s) got set.
    
    (issue ASTERISK-22872)
    Reported by: Matt Jordan
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@4442 a5964663-950a-4945-9369-5dd0b5cb08c1

---
+++ tests/rest_api/channels/originate_with_vars/originate_with_vars.py
@@ -0,0 +1,30 @@
+'''
+Copyright (C) 2013, Digium, Inc.
+Kevin Harwell <kharwell@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+'''
+
+import json
+import requests
+
+def on_start(ari, event, obj):
+    url = ari.build_url('channels')
+
+    params = {
+        'endpoint': 'Local/1000@default',
+        'app':'testsuite',
+        'appArgs': 'with_vars'}
+
+    data = {'CALLERID(name)': 'foo'}
+    headers = {'Content-type': 'application/json'}
+
+    resp = requests.post(url, params=params, data=json.dumps(data),
+                         headers=headers, auth=ari.userpass)
+
+    assert resp.json()['caller']['name'] == 'foo'
+    ari.delete('channels', resp.json()['id'])
+
+    ari.delete('channels', event['channel']['id'])
+    return True
