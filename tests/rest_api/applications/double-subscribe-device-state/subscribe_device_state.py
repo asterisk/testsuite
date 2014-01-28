d4e88aa4ae3673a40d7e98dbb9c6e0fb668be08d
Kinsey Moore <kmoore@digium.com>
2014-01-28 19:21:22 +0000

    TestSuite: Add ARI test for double subscription
    This adds a test to verify that a second subscription attempt to a
    device state does not cause an error response.
    
    (closes issue ASTERISK-23143)
    Reported by: Matt Jordan
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@4611 a5964663-950a-4945-9369-5dd0b5cb08c1

---
+++ tests/rest_api/applications/double-subscribe-device-state/subscribe_device_state.py
@@ -0,0 +1,42 @@
+"""
+Copyright (C) 2013, Digium, Inc.
+Kevin Harwell <kharwell@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+"""
+
+URL = 'deviceStates'
+DEVICE = 'Stasis:Test'
+INITIAL_STATE = 'NOT_INUSE'
+CHANGED_STATE = 'INUSE'
+
+def on_start(ari, event, obj):
+    # add a device state
+    ari.put(URL, DEVICE, deviceState=INITIAL_STATE)
+
+    # subscribe to device
+    ari.post("applications", "testsuite", "subscription",
+             eventSource="deviceState:%s" % DEVICE)
+
+    # subscribe to device
+    ari.post("applications", "testsuite", "subscription",
+             eventSource="deviceState:%s" % DEVICE)
+
+    # change the device state
+    ari.put(URL, DEVICE, deviceState=CHANGED_STATE)
+
+    # unsubscribe from device
+    ari.delete("applications", "testsuite", "subscription",
+             eventSource="deviceState:%s" % DEVICE)
+
+    # remove device
+    ari.delete(URL, DEVICE)
+
+    ari.delete('channels', event['channel']['id'])
+    return True
+
+def on_state_change(ari, event, obj):
+    assert event['device_state']['name'] == DEVICE
+    assert event['device_state']['state'] == CHANGED_STATE
+    return True
