585a002be4b34ba1743e36741e3e175a8aeca31f
Jonathan Rose <jrose@digium.com>
2014-05-08 22:09:17 +0000

    Testsuite: ARI playback for tones on channels and bridges (also with tonezone)
    (closes issue ASTERISK-23433)
    Reported by: Matt Jordan
    Review: https://reviewboard.asterisk.org/r/3428/
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@5026 a5964663-950a-4945-9369-5dd0b5cb08c1

---
+++ tests/rest_api/bridges/playback/tones/bridges_play.py
@@ -0,0 +1,50 @@
+'''
+Copyright (C) 2014, Digium, Inc.
+Jonathan R. Rose <jrose@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+'''
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+class SubscribeBridge(object):
+    def __init__(self):
+        self.channel_id = None
+
+BRIDGE_ID = 'test-tones-bridge'
+TEST = SubscribeBridge()
+
+def on_start(ari, event, test_object):
+    """ When the channel enters stasis, create a bridge and add the channel. """
+    TEST.channel_id = event['channel']['id']
+
+    ari.post('bridges',
+             bridgeId=BRIDGE_ID)
+
+    ari.post('bridges', BRIDGE_ID, 'addChannel',
+             channel=TEST.channel_id)
+
+    return True
+
+
+def on_playback_finished(ari, event, test_object):
+    """ When the PlaybackFinished event is received, remove the channel. """
+    ari.post('bridges', BRIDGE_ID, 'removeChannel',
+             channel=TEST.channel_id)
+
+    return True
+
+
+def on_channel_left_bridge(ari, event, test_object):
+    """ testsuite received a ChannelLeftBridge event, wrap the test up. """
+    assert BRIDGE_ID == event['bridge']['id']
+    assert TEST.channel_id == event['channel']['id']
+
+    ari.delete('channels', TEST.channel_id)
+    ari.delete('bridges', BRIDGE_ID)
+
+    return True
