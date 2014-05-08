9ef32534930bbba1aa77866e8fe745da61e021c1
Kinsey Moore <kmoore@digium.com>
2014-05-08 14:39:44 +0000

    Testsuite: Simplify REST Attended Transfer test
    This simplifies the REST attended transfer test in preparation for a
    partial rewrite. Functionality is unchanged other than removing the
    creation of a channel that was not actually needed.
    
    
    git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@5020 a5964663-950a-4945-9369-5dd0b5cb08c1

--- tests/rest_api/bridges/attended_transfer/attended_transfer.py
+++ tests/rest_api/bridges/attended_transfer/attended_transfer.py
@@ -13,62 +13,55 @@ import time
 LOGGER = logging.getLogger(__name__)
 
 class TestLogic(object):
-	def __init__(self):
-		self.channels = 0
-		self.bridge_id = None
-		self.pja = subprocess.Popen(['pjsua', '--local-port=5065', '--null-audio',
-			'--id=sip:bob@127.0.0.1'],
-			stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-		self.pjb = subprocess.Popen(['pjsua', '--local-port=5066', '--null-audio',
-			'--id=sip:alice@127.0.0.1'],
-			stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    def __init__(self):
+        self.channels = 0
+        self.originated_id = None
+        self.bridge_id = None
+        self.pja = subprocess.Popen(['pjsua', '--local-port=5065', '--null-audio',
+            '--id=sip:bob@127.0.0.1'],
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
 TEST = TestLogic()
 
 def a_call_stasis():
-	TEST.pja.stdin.write("m\n")
-	TEST.pja.stdin.write("sip:stasis@127.0.0.1:5060\n")
-
-def b_call_stasis():
-	TEST.pjb.stdin.write("m\n")
-	TEST.pjb.stdin.write("sip:stasis@127.0.0.1:5060\n")
+    TEST.pja.stdin.write("m\n")
+    TEST.pja.stdin.write("sip:stasis@127.0.0.1:5060\n")
 
 def on_third_leg(ami, event):
-	TEST.pja.stdin.write("X\n")
-	TEST.pja.stdin.write("1\n")
-	return True
+    LOGGER.debug("Peforming transfer")
+    TEST.pja.stdin.write("X\n")
+    LOGGER.debug("Selecting transferee")
+    TEST.pja.stdin.write("1\n")
+    LOGGER.debug("DONE")
+    return True
 
 def a_call_app():
-	TEST.pja.stdin.write("H\n")
-	TEST.pja.stdin.write("m\n")
-	TEST.pja.stdin.write("sip:1000@127.0.0.1:5060\n")
+    TEST.pja.stdin.write("H\n")
+    TEST.pja.stdin.write("m\n")
+    TEST.pja.stdin.write("sip:1000@127.0.0.1:5060\n")
 
 def on_kickoff_start(ari, event, test_object):
     LOGGER.debug("on_kickoff_start(%r)" % event)
     TEST.bridge_id = ari.post('bridges').json()['id']
     a_call_stasis()
-    b_call_stasis()
-    ari.delete('channels', event['channel']['id'])
+    TEST.originated_id = event['channel']['id']
+    ari.post('bridges', TEST.bridge_id, 'addChannel', channel=event['channel']['id'])
     return True
 
 def on_test_start(ari, event, test_object):
     LOGGER.debug("on_test_start(%r)" % event)
     ari.post('bridges', TEST.bridge_id, 'addChannel', channel=event['channel']['id'])
-
-    TEST.channels += 1
-    if TEST.channels == 2:
-        a_call_app()
-
+    a_call_app()
     return True
 
 def on_attended_transfer(ari, event, test_object):
-	LOGGER.debug("on_attended_transfer(%r)" % event)
-	ari.delete('bridges', TEST.bridge_id)
-	TEST.pjb.stdin.write("h\n")
+    LOGGER.debug("on_attended_transfer(%r)" % event)
+    ari.delete('bridges', TEST.bridge_id)
+    ari.delete('channels', TEST.originated_id)
 
-	if not event['transferer_first_leg']['name'].startswith('PJSIP/bob-'):
-		return False
-	elif event['transferer_first_leg_bridge']['id'] != TEST.bridge_id:
-		return False
+    if not event['transferer_first_leg']['name'].startswith('PJSIP/bob-'):
+        return False
+    elif event['transferer_first_leg_bridge']['id'] != TEST.bridge_id:
+        return False
 
-	return True
+    return True
