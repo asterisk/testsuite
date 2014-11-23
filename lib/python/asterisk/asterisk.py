asterisk/testsuite

commit 0f9fbe25d6b2c0a17cdf73975d1e716707c7658f

testsuite: add basic valgrind support
This adds very basic support for running Asterisk
instances under Valgrind, which is convenient for
manual test runs but does not yet include parsing
the output for failures.

1) Testsuite CLI flag '-V' enables valgrind:
   ./runtests -V -t tests/test...

2) Valgrind output is picked up by error logging
   and shown after test run.

3) Works with multiple Asterisk instances.

Reviewboard: https://reviewboard.asterisk.org/r/4090/



git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@5942 a5964663-950a-4945-9369-5dd0b5cb08c1

lib/python/asterisk/asterisk.py
@@ -332,7 +332,7 @@ class Asterisk(object):
         def __wait_fully_booted_error(cli_command):
             """Errback for CLI command waitfullybooted"""
 
-            if time.time() - self.__start_asterisk_time > 5:
+            if time.time() - self.__start_asterisk_time > 90:
                 msg = "Asterisk core waitfullybooted for %s failed" % self.host 
                 LOGGER.error(msg)
                 self._start_deferred.errback(Exception(msg))
@@ -350,6 +350,14 @@ class Asterisk(object):
             "-f", "-g", "-q", "-m", "-n",
             "-C", "%s" % os.path.join(self.astetcdir, "asterisk.conf")
         ]
+
+        if os.getenv("VALGRIND_ENABLE") == "true":
+            valgrind_path = test_suite_utils.which('valgrind')
+            if valgrind_path:
+                cmd = [valgrind_path] + cmd
+            else:
+                LOGGER.error('Valgrind not found')
+
 
         # Make the start/stop deferreds - this method will return
         # the start deferred, and pass the stop deferred to the AsteriskProtocol
@@ -455,7 +463,7 @@ class Asterisk(object):
         else:
             # Schedule a kill. If we don't gracefully shut down Asterisk, this
             # will ensure that the test is stopped.
-            self._stop_cancel_tokens.append(reactor.callLater(10, __send_kill))
+            self._stop_cancel_tokens.append(reactor.callLater(200, __send_kill))
 
             # Start by asking to stop gracefully.
             __send_stop_gracefully()

lib/python/asterisk/test_case.py
@@ -138,6 +138,9 @@ class TestCase(object):
         self._pcap_callbacks = []
         self._stop_deferred = None
 
+        if os.getenv("VALGRIND_ENABLE") == "true":
+            self.reactor_timeout *= 20
+
         # Pull additional configuration from YAML config if possible
         if test_config:
             if 'reactor-timeout' in test_config:

runtests.py
@@ -497,6 +497,9 @@ def main(argv=None):
     parser.add_option("-n", "--dry-run", action="store_true",
             dest="dry_run", default=False,
             help="Only show which tests would be run.")
+    parser.add_option("-V", "--valgrind", action="store_true",
+            dest="valgrind", default=False,
+            help="Run Asterisk under Valgrind")
     (options, args) = parser.parse_args(argv)
 
     ast_version = AsteriskVersion(options.version)
@@ -516,6 +519,9 @@ def main(argv=None):
     if options.list_tags:
         test_suite.list_tags()
         return 0
+
+    if options.valgrind:
+        os.environ["VALGRIND_ENABLE"] = "true"
 
     print "Running tests for Asterisk %s ...\n" % str(ast_version)
 

commit 4537f826d55af24ea0bae7ad50060bc49f020336

asterisk: Fix timeout issues introduced by r5942
When the valgrind patch was merged for r5942, it unilaterally increased the
timeouts for the callback sequence during an Asterisk shutdown. When we
shutdown Asterisk, we:
* Call stop gracefully
* Wait a bit, then call stop now
* Wait a bit, then kill Asterisk

The wait times for killing Asterisk was increased from 5 seconds to 200
seconds. For tests that currently KILL Asterisk, this made them run
inordinately long. It also increased unilaterally the 'core wait fullybooted'
timeout.

This patch restores the regular timeouts when VALGRIND_ENABLED is not detected.


git-svn-id: https://origsvn.digium.com/svn/testsuite/asterisk/trunk@5993 a5964663-950a-4945-9369-5dd0b5cb08c1

lib/python/asterisk/asterisk.py
@@ -231,6 +231,9 @@ class Asterisk(object):
         self.astetcdir = ""
         self.original_astmoddir = ""
 
+        valgrind_env = os.getenv("VALGRIND_ENABLE") or ""
+        self.valgrind_enabled = True if "true" in valgrind_env else False
+
         if base is not None:
             self.base = base
         else:
@@ -332,8 +335,9 @@ class Asterisk(object):
         def __wait_fully_booted_error(cli_command):
             """Errback for CLI command waitfullybooted"""
 
-            if time.time() - self.__start_asterisk_time > 90:
-                msg = "Asterisk core waitfullybooted for %s failed" % self.host 
+            timeout = 90 if self.valgrind_enabled else 5
+            if time.time() - self.__start_asterisk_time > timeout:
+                msg = "Asterisk core waitfullybooted for %s failed" % self.host
                 LOGGER.error(msg)
                 self._start_deferred.errback(Exception(msg))
             else:
@@ -357,7 +361,6 @@ class Asterisk(object):
                 cmd = [valgrind_path] + cmd
             else:
                 LOGGER.error('Valgrind not found')
-
 
         # Make the start/stop deferreds - this method will return
         # the start deferred, and pass the stop deferred to the AsteriskProtocol
@@ -459,11 +462,13 @@ class Asterisk(object):
                         "Asterisk %s stopped prematurely" % self.host)
             except defer.AlreadyCalledError:
                 LOGGER.warning("Asterisk %s stop deferred already called" %
-                    self.host)
+                               self.host)
         else:
             # Schedule a kill. If we don't gracefully shut down Asterisk, this
             # will ensure that the test is stopped.
-            self._stop_cancel_tokens.append(reactor.callLater(200, __send_kill))
+            sched_time = 200 if self.valgrind_enabled else 5
+            self._stop_cancel_tokens.append(reactor.callLater(sched_time,
+                                            __send_kill))
 
             # Start by asking to stop gracefully.
             __send_stop_gracefully()