62fce095e0c5c80d41ddebacf035374128dee850
Jonathan Rose <jrose@digium.com>
2015-06-04 12:27:03 -0500

    channels/pjsip/subscriptions/rls - Improve logging
    The primary purpose of this is to improve logging in the RLS tests so that
    the exact phases of the test and expectations being evaluated are known
    at any given time when reviewing logs. In addition, some improvements have
    been made to the integrity checks so that failures are reported back to the
    rls_test module.
    
    ASTERISK-25144
    Reported by: Jonathan Rose
    
    Change-Id: Iea3f5bb8a424d7ff813a85d6c8224a14ebc3e0dd

--- tests/channels/pjsip/subscriptions/rls/rls_test.py
+++ tests/channels/pjsip/subscriptions/rls/rls_test.py
@@ -1,4 +1,11 @@
 #/usr/bin/env python
+"""
+Copyright (C) 2015, Digium, Inc.
+Jonathan Rose <jrose@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+"""
 
 import sys
 import logging
@@ -12,14 +19,22 @@ from twisted.internet import reactor
 
 LOGGER = logging.getLogger(__name__)
 
+
 class IntegrityCheck(VOIPListener):
-    """A test module that observes incoming SIP notifies and compares them
+    """Verifies that SIP notifies contain expected updates
+
+       A test module that observes incoming SIP notifies and compares them
        to a set of expected results. Tests may optionally specify for an
        arbitrary number of AMI actions to be executed in order at 2 second
        intervals from the start of the test.
     """
     def __init__(self, module_config, test_object):
-        """Create listener and add AMI observer/callbacks."""
+        """Constructor
+
+        Arguments:
+        module_config Dictionary containing test configuration
+        test_object used to manipulate reactor and set/remove failure tokens
+        """
         self.set_pcap_defaults(module_config)
         VOIPListener.__init__(self, module_config, test_object)
 
@@ -42,8 +57,7 @@ class IntegrityCheck(VOIPListener):
         self.add_callback('SIP', self.multipart_handler)
 
     def set_pcap_defaults(self, module_config):
-        """Set default values for PcapListener module for values that aren't
-           explicitly overridden.
+        """Sets default values for the PcapListener
         """
         if not module_config.get('bpf-filter'):
             module_config['bpf-filter'] = 'udp port 5061'
@@ -55,16 +69,31 @@ class IntegrityCheck(VOIPListener):
             module_config['device'] = 'lo'
 
     def multipart_handler(self, packet):
-        """Checks multipart packets to see if NOTIFY messages have
-           anticipated data.
+        """Handles incoming SIP packets and verifies contents
+
+        Checks to see if a packet is a NOTIFY packet with a multipart body.
+        Provided that all the basic checks pass, creates and RLSValidator for
+        the packet that will verify that the contents of the NOTIFY match the
+        expectations set for NOTIFY in the test configuration.
+
+        Arguments:
+        packet Incoming SIP Packet
         """
+
+        LOGGER.debug('Received SIP packet')
+
         if 'NOTIFY' not in packet.request_line:
+            LOGGER.debug('Ignoring packet, is not a NOTIFY')
             return
 
         if packet.body.packet_type != 'Multipart':
+            LOGGER.debug('Ignoring packet, NOTIFY does not contain ' +
+                         'multipart body')
             return
 
         if self.version >= len(self.resources):
+            LOGGER.debug('Ignoring packet, version is higher than count of ' +
+                         'test expectations')
             return
 
         validator = RLSValidator(test_object=self.test_object,
@@ -73,10 +102,20 @@ class IntegrityCheck(VOIPListener):
                                  full_state=self.full_state[self.version],
                                  list_name=self.list_name,
                                  resources=self.resources[self.version])
-        validator.check_integrity()
+
+        debug_msg = "validating packet -- expecting {0}"
+        LOGGER.debug(debug_msg.format(self.resources[self.version]))
+        if not validator.check_integrity():
+            LOGGER.error('Integrity Check Failed.')
+            return
+
+        info_msg = "Packet validated successfully. Test Phase {0} Completed."
+        LOGGER.info(info_msg.format(self.version))
         self.version += 1
 
         if self.version == len(self.resources):
+            info_msg = "All test phases completed. RLS verification complete."
+            LOGGER.info(info_msg)
             self.test_object.remove_fail_token(self.token)
             if self.stop_after_notifys:
                 # We only deal with as many NOTIFIES as we have resources
@@ -90,10 +129,19 @@ class IntegrityCheck(VOIPListener):
     def scenario_started(self, scenario):
         """Callback when SIPp scenario has started.
 
-        If this test executes an AMI action, this
+        If this test executes AMI actions, this function will execute
+        those AMI actions at two second intervals.
+
+        Note: Overrides scenario_started from VOIPListener
+
+        Arguments:
+        scenario Not actually used, just part of the signature.
         """
         def _perform_ami_action():
-            self.ami.sendMessage(self.ami_action.pop(0))
+            action = self.ami_action.pop(0)
+            debug_msg = "Sending AMI action: {0}"
+            LOGGER.debug(debug_msg.format(action))
+            self.ami.sendMessage(action)
             if len(self.ami_action):
                 reactor.callLater(2, _perform_ami_action)
 
