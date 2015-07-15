4f66e2a8622f9b6bf2342d20cd5ee4754e57ae64
Mark Michelson <mmichelson@digium.com>
2015-07-15 14:22:28 -0500

    res_pjsip: Add direct media rtp_keepalive test
    This test has two SIPp scenarios bridged together in Asterisk with
    direct media enabled. The test ensures that when direct media is in use,
    Asterisk does not send any RTP keepalive packets to the endpoints.
    
    ASTERISK-25242
    Reported by Mark Michelson
    
    Change-Id: I23009970201cc07701f8208a652b682f64c11537

---
+++ tests/channels/pjsip/rtp/rtp_keepalive/direct_media/rtp.py
@@ -0,0 +1,27 @@
+'''
+Copyright (C) 2015, Digium, Inc.
+Mark Michelson <mmichelson@digium.com>
+
+This program is free software, distributed under the terms of
+the GNU General Public License Version 2.
+'''
+import logging
+
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+LOGGER = logging.getLogger(__name__)
+
+class RTP(DatagramProtocol):
+    def __init__(self, test_object):
+        self.test_object = test_object
+
+    def datagramReceived(self, data, (host, port)):
+        LOGGER.error("Received RTP from Asterisk unexpectedly")
+        self.test_object.set_passed(False)
+        self.test_object.stop_reactor()
+
+class KeepaliveCheck(object):
+    def __init__(self, module_config, test_object):
+        reactor.listenUDP(25552, RTP(test_object))
+        reactor.listenUDP(55225, RTP(test_object))
