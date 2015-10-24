53d6d181fda52b801a2b731f22ade151ed63e442
Matt Jordan <mjordan@digium.com>
2015-10-24 08:42:50 -0500

    test_case: Allow the test config for TestCaseModule to be empty
    TestCaseModule does not require any configuration, although it is relatively
    boring in that case. Either way, the constructor for the class should be
    tolerant of an empty configuration object.
    
    Change-Id: I646ed342a47343dfd6f07b5f8f56cd9fb8b10091

--- lib/python/asterisk/test_case.py
+++ lib/python/asterisk/test_case.py
@@ -889,6 +889,10 @@ class TestCaseModule(TestCase):
         :param test_config The YAML provided test configuration for this object
         """
         super(TestCaseModule, self).__init__(test_path, test_config)
+
+        if not test_config:
+            test_config = {}
+
         self.asterisk_instances = test_config.get('asterisk-instances') or 1
         self.connect_ami = test_config.get('connect-ami') or False
         self.connect_agi = test_config.get('connect-agi') or False
