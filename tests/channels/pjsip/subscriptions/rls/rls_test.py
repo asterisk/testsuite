56491e4d7a00917bc4bdcf770a469c4220e8516f
Ashley Sanders <asanders@digium.com>
2015-10-26 13:51:17 -0500

    Testsuite: batched rls subscription failure
    While this does not actually correct the issue with the test failure (which
    incidentally turns out is not localized to this test), this patch modifies the
    the test validator by separating the concerns for each validation method,
    improving the logging, and fixing pylint issues.
    
    This is part four (4) of an n-patch series of refactorings to help determine the
    root cause of the test failure and correct pylint issues.
    
    ASTERISK-25430
    Reported By: Ashley Sanders
    
    Change-Id: Icb8af439f5c3db3efb0a93d6402ab4dcacdf3731

--- tests/channels/pjsip/subscriptions/rls/rls_test.py
+++ tests/channels/pjsip/subscriptions/rls/rls_test.py
@@ -10,14 +10,78 @@ the GNU General Public License Version 2.
 import sys
 import logging
 
-sys.path.append('lib/python')
-sys.path.append('tests/channels/pjsip/subscriptions/rls')
+sys.path.append("lib/python")
+sys.path.append("tests/channels/pjsip/subscriptions/rls")
 
 from pcap import VOIPListener
-from rls_integrity import RLSValidator
+from rls_element import RLSPacket
+from rls_validation import ValidationInfo
 from twisted.internet import reactor
 
 LOGGER = logging.getLogger(__name__)
+
+def filter_multipart_packet(packet):
+    """Determines if a packet is an RLS multipart packet.
+
+    Keyword Arguments:
+    packet                  -- A yappcap.PcapPacket
+
+    Returns:
+    True if this is a multipart NOTIFY packet. Otherwise, returns False.
+    """
+
+    # If the packet is not a SIP NOTIFY, this is not a packet we care
+    # about.
+    if "NOTIFY" not in packet.request_line:
+        LOGGER.debug("Ignoring packet, is not a NOTIFY")
+        return False
+
+    # If the packet body is not a multipart packet body, this is not a
+    # packet we care about.
+    if packet.body.packet_type != "Multipart":
+        LOGGER.debug("Ignoring packet, NOTIFY does not contain " \
+                     "multipart body")
+        return False
+
+    return True
+
+def log_packet(packet, write_packet_contents):
+    """Writes the contents of a SIP packet to the log.
+
+    Keyword Arguments:
+    packet                  -- A yappcap.PcapPacket
+    write_packet_contents   -- Whether or not to dump the contents of the
+                               packet to the log.
+    """
+
+    message = "Received SIP packet:\n"
+
+    if not write_packet_contents:
+        return
+
+    for attr in dir(packet):
+        message += "%s = %s\n" % (attr, getattr(packet, attr))
+        if attr == "body":
+            message += "body:\n"
+            body = getattr(packet, attr)
+            for body_attr in dir(body):
+                value = getattr(body, body_attr)
+                message += "\t%s = %s\n" % (body_attr, value)
+                message += "%s = %s\n" % (attr, getattr(packet, attr))
+
+    LOGGER.debug(message)
+
+def set_pcap_defaults(module_config):
+    """Sets default values for the PcapListener."""
+
+    if not module_config.get("bpf-filter"):
+        module_config["bpf-filter"] = "udp port 5061"
+    if not module_config.get("register-observer"):
+        module_config["register-observer"] = True
+    if not module_config.get("debug-packets"):
+        module_config["debug-packets"] = True
+    if not module_config.get("device"):
+        module_config["device"] = "lo"
 
 
 class RLSTest(VOIPListener):
@@ -37,41 +101,72 @@ class RLSTest(VOIPListener):
         test_object            -- Used to manipulate reactor and set/remove
                                   failure tokens.
         """
-        self.set_pcap_defaults(module_config)
-        VOIPListener.__init__(self, module_config, test_object)
+
+        set_pcap_defaults(module_config)
+        super(RLSTest, self).__init__(module_config, test_object)
 
         self.test_object = test_object
-        self.token = test_object.create_fail_token("Haven't handled all "
-                                                   "expected NOTIFY packets.")
+        self.ami_action = module_config.get("ami-action")
 
-
-        self.list_name = module_config['list-name']
-        self.log_packets = module_config.get("log-packets", False)
-        self.packets = module_config['packets']
-        self.ami_action = module_config.get('ami-action')
-        self.stop_test_after_notifys = \
-            module_config.get("stop-test-after-notifys", True)
-
+        if self.check_prerequisites():
+            self.test_object.register_scenario_started_observer(
+                self.on_scenario_started)
 
         self.ami = None
         self.packets_idx = 0
-        self.test_object.register_ami_observer(self.ami_connect)
-        if hasattr(self.test_object, 'register_scenario_started_observer'):
-            self.test_object.register_scenario_started_observer(
-                self.scenario_started)
-        self.add_callback('SIP', self.multipart_handler)
+        self.list_name = module_config["list-name"]
+        self.log_packets = module_config.get("log-packets", False)
+        self.packets = module_config["packets"]
 
-    def set_pcap_defaults(self, module_config):
-        """Sets default values for the PcapListener
+        self.stop_test_after_notifys = \
+            module_config.get("stop-test-after-notifys", True)
+
+        token_msg = "Test execution was interrupted before all NOTIFY " \
+                    "packets were accounted for."
+        self.token = self.test_object.create_fail_token(token_msg)
+
+        self.test_object.register_ami_observer(self.on_ami_connect)
+        self.add_callback("SIP", self.multipart_handler)
+
+    def check_prerequisites(self):
+        """Checks the test_object can support an ami_action, if configured.
+
+        Note: This method will fail the test if it is determined that the
+        test has a dependency on an ami_action but uses a test object that
+        does not have a definition for 'register_scenario_started_observer'.
+
+        Returns:
+        True if the test object supports 'register_scenario_started_observer'.
+        Otherwise, returns False.
         """
-        if not module_config.get('bpf-filter'):
-            module_config['bpf-filter'] = 'udp port 5061'
-        if not module_config.get('register-observer'):
-            module_config['register-observer'] = True
-        if not module_config.get('debug-packets'):
-            module_config['debug-packets'] = True
-        if not module_config.get('device'):
-            module_config['device'] = 'lo'
+
+        is_start_observer = hasattr(self.test_object,
+                                    "register_scenario_started_observer")
+        if self.ami_action is not None:
+            message = "This test is configured with an ami_action " \
+                      "attribute. However, it is also configured to " \
+                      "use a test-object that does not contain support " \
+                      "for 'register_scenario_started_observer'. As a " \
+                      "result, the ami_action will never be executed. " \
+                      "Either reconfigure the test to run without a " \
+                      "dependency for an ami_action or change the " \
+                      "test-object type to a type that supports " \
+                      "'register_scenario_started_observer'."
+            self.fail_test(message)
+
+        return is_start_observer
+
+    def fail_test(self, message):
+        """Marks the test as failed and stops the reactor.
+
+        Keyword Arguments:
+        message                -- Reason for the test failure"""
+
+        LOGGER.error(message)
+        self.test_object.remove_fail_token(self.token)
+        self.token = self.test_object.create_fail_token(message)
+        self.test_object.set_passed(False)
+        self.test_object.stop_reactor()
 
     def multipart_handler(self, packet):
         """Handles incoming SIP packets and verifies contents
@@ -85,39 +180,36 @@ class RLSTest(VOIPListener):
         packet                 -- Incoming SIP Packet
         """
 
-        LOGGER.debug('Received SIP packet')
+        log_packet(packet, self.log_packets)
 
-        if 'NOTIFY' not in packet.request_line:
-            LOGGER.debug('Ignoring packet, is not a NOTIFY')
-            return
-
-        if packet.body.packet_type != 'Multipart':
-            LOGGER.debug('Ignoring packet, NOTIFY does not contain ' +
-                         'multipart body')
+        if not filter_multipart_packet(packet):
             return
 
         if self.packets_idx >= len(self.packets):
-            LOGGER.debug('Ignoring packet, version is higher than count of ' +
-                         'test expectations')
-            return
+            message = "Received more packets ({0}) than expected ({1}). " \
+                      "Failing test.".format(self.packets_idx,
+                                             len(self.packets))
+            self.fail_test(message)
 
+        rls_packet = RLSPacket(packet)
         resources = self.packets[self.packets_idx]["resources"]
-        full_state = self.packets[self.packets_idx]["full-state"]
+        fullstate = self.packets[self.packets_idx]["full-state"]
 
-        validator = RLSValidator(test_object=self.test_object,
-                                 packet=packet,
-                                 version=self.packets_idx,
-                                 full_state=full_state,
-                                 list_name=self.list_name,
-                                 resources=resources)
+        info = ValidationInfo(resources=resources,
+                              version=self.packets_idx,
+                              fullstate=fullstate,
+                              rlmi=None,
+                              rlmi_name=self.list_name)
 
-        debug_msg = "validating packet -- expecting {0}"
-        LOGGER.debug(debug_msg.format(self.packets[self.packets_idx]))
-        if not validator.check_integrity():
-            LOGGER.error('Integrity Check Failed.')
+        message = "Validating Resource ({0}) of ({1})..."
+        LOGGER.debug(message.format(self.packets_idx, len(self.packets) - 1))
+
+        if not rls_packet.validate(info):
+            message = "Integrity Check Failed for Resource ({0})."
+            self.fail_test(message.format(self.packets_idx))
             return
 
-        info_msg = "Packet validated successfully. Test Phase {0} Completed."
+        info_msg = "Resource ({0}) validated successfully."
         LOGGER.info(info_msg.format(self.packets_idx))
         self.packets_idx += 1
 
@@ -125,17 +217,17 @@ class RLSTest(VOIPListener):
             info_msg = "All test phases completed. RLS verification complete."
             LOGGER.info(info_msg)
             self.test_object.remove_fail_token(self.token)
-            if self.stop_after_notifys:
+            if self.stop_test_after_notifys:
                 # We only deal with as many NOTIFIES as we have defined in our
                 # test-config.yaml
                 self.test_object.set_passed(True)
                 self.test_object.stop_reactor()
 
-    def ami_connect(self, ami):
+    def on_ami_connect(self, ami):
         """Callback when AMI connects. Sets test AMI instance."""
         self.ami = ami
 
-    def scenario_started(self, scenario):
+    def on_scenario_started(self, scenario):
         """Callback when SIPp scenario has started.
 
         If this test executes AMI actions, this function will execute
