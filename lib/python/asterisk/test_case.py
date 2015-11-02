860e8e429accb7cb930b4a75cd478e688cabfb2f
Matt Jordan <mjordan@digium.com>
2015-11-02 06:18:11 -0600

    test_case: Fallback to non-reconnecting AMIFactory
    When c27a4774 was merged, it accidentally removed the fallback to a
    non-reconnecting AMIFactory if the reconnecting version is not supported
    by the underlying starpy library. This patch adds that back in.
    
    ASTERISK-25515 #close
    
    Change-Id: I81a79953ed7e2ed67195b2ab4c2ccf9e29f6d9a5

--- lib/python/asterisk/test_case.py
+++ lib/python/asterisk/test_case.py
@@ -302,8 +302,12 @@ class TestCase(object):
 
             self.ami.append(None)
             LOGGER.info("Creating AMIFactory %d to %s" % ((i + 1), host))
-            ami_factory = manager.AMIFactory(actual_user, actual_secret, i,
-                                             on_reconnect=on_reconnect)
+            try:
+                ami_factory = manager.AMIFactory(actual_user, actual_secret, i,
+                                                 on_reconnect=on_reconnect)
+            except:
+                # Try without reconnects
+                ami_factory = manager.AMIFactory(actual_user, actual_secret, i)
             deferred = ami_factory.login(ip=host, port=actual_port)
             deferred.addCallbacks(self._ami_connect, self.ami_login_error)
 
