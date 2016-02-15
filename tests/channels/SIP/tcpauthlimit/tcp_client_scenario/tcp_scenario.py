d33c424b961af27ff05bffb4169e12371cd454f2
Mark Michelson <mmichelson@digium.com>
2016-02-15 17:49:44 -0600

    Fix failing tcpauthlimit TCP scenario test.
    The existing tcpauthlimit test was bouncing in CI test runs. The test
    created TCP connections to Asterisk. It would then attempt to write
    data to the socket and read data from the socket. If the read attempts
    returned the EWOULDBLOCK error, then the connection was marked as closed
    by the remote peer. If the read attempts succeeded, then the connection
    was marked as connected.
    
    The problem with this is that it does not accurately reflect what is
    happening under the hood. Getting EWOULDBLOCK does not indicate that the
    connection is closed; it just means that the connection never had data
    to read when we checked. Further, not getting EWOULDBLOCK did not
    necessarily indicate that the connection was up, since a common way that
    TCP indicates that the remote peer closed the connection is to have the
    socket ready for reading but return 0 bytes of data when attempting to
    read. As such, I had local test runs where the test run assumed that
    certain connections to Asterisk were up, but I could see from some added
    debug in Asterisk that those connections were ones that Asterisk had
    closed. I saw the reverse scenario as well.
    
    The existing test was a bit complex for what it was trying to do, so
    rather than trying to patch the existing test, I have opted to rewrite
    it. The new approach is to use the twisted.protocol's connectionLost()
    method to detect when the connection is lost. The scenario is also
    simplified to use fewer connections, so it is easy to track what is
    happening during a test.
    
    Change-Id: Ie776e62288bde454754a35840aa870e07a452555

---
+++ tests/channels/SIP/tcpauthlimit/tcp_client_scenario/tcp_scenario.py
@@ -0,0 +1,59 @@
+import logging
+from twisted.internet import reactor
+from twisted.internet.protocol import ClientFactory, Protocol
+
+LOGGER = logging.getLogger(__name__)
+
+AUTH_LIMIT = 1
+EXPECTED_CONNECTIONS_MADE = 2
+EXPECTED_CONNECTIONS_LOST = EXPECTED_CONNECTIONS_MADE - AUTH_LIMIT
+
+
+class TCPClient(Protocol):
+    def __init__(self, module, port):
+        self.module = module
+        self.port = port
+
+    def connectionLost(self, reason):
+        LOGGER.info("Lost connection for source port {0}".format(self.port))
+        LOGGER.info("Reason is {0}".format(reason.value))
+        self.module.connections_lost += 1
+
+    def connectionMade(self):
+        LOGGER.info("Connection made for source port {0}".format(self.port))
+        self.module.connections_made += 1
+
+
+class TCPClientFactory(ClientFactory):
+    def __init__(self, module, port):
+        self.module = module
+        self.port = port
+
+    def buildProtocol(self, addr):
+        LOGGER.info("Building protocol for source port {0}".format(self.port))
+        return TCPClient(self.module, self.port)
+
+
+class TCPClientModule(object):
+    def __init__(self, test_config, test_object):
+        self.test_object = test_object
+        self.connections_made = 0
+        self.connections_lost = 0
+        test_object.register_ami_observer(self.ami_connect)
+
+    def ami_connect(self, ami):
+        reactor.connectTCP("127.0.0.1", 5060, TCPClientFactory(self, 5062),
+                           bindAddress=("127.0.0.1", 5062))
+        reactor.connectTCP("127.0.0.1", 5060, TCPClientFactory(self, 5063),
+                           bindAddress=("127.0.0.1", 5063))
+        reactor.callLater(10, self.evaluate_connections)
+
+    def evaluate_connections(self):
+        LOGGER.info("Made {0} connections".format(self.connections_made))
+        LOGGER.info("Lost {0} connections".format(self.connections_lost))
+        if (self.connections_made == EXPECTED_CONNECTIONS_MADE and
+            self.connections_lost == EXPECTED_CONNECTIONS_LOST):
+            self.test_object.set_passed(True)
+        else:
+            self.test_object.set_passed(False)
+        self.test_object.stop_reactor()
