5c4b250e3eb50700404605c1b59ee577ebed1b2e
Kevin Harwell <kharwell@digium.com>
2018-06-27 10:57:49 -0500

    cleanup-test-remnants: Remove *.pyc files and __pycache__ directories
    __pycache__ should only contain *.pyc files, but made it so the whole directory
    is removed anyways. Also added it to the .gitignore.
    
    Change-Id: I2074a746f747159b2cca23b40359a19ccec2218c

--- cleanup-test-remnants.sh
+++ cleanup-test-remnants.sh
@@ -2,3 +2,5 @@
 
 find . -type d -name tmp | grep -vF .svn | xargs -d\\n rm -rf
 rm -rf /tmp/asterisk-testsuite /var/tmp/asterisk-testsuite ./logs/* ./fastagi
+find -name '__pycache__' -type d -exec rm -rf {} +
+find -name '*.pyc' -delete
