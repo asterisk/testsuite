16cd406615ce90563bc4a2c11d4912f7a36e5621
Richard Mudgett <rmudgett@digium.com>
2018-07-27 14:37:26 -0500

    fax/pjsip/t38_fast_reject: Fix logs left in testsuite tree
    We don't need to pollute the testsuite tree with log files that are not
    needed.
    
    In run-test:
    * Don't generate log files that are not needed.
    * Fix to use a temp file out of the testsuite tree on the only SIPp log
    file needed so it is not left in the testsuite tree.
    * Fixed PEP8 errors
    
    In A_PARTY.xml
    * Removed extra optional recv 100 msgs.
    * Fixed indention
    
    In B_PARTY.xml
    * Fix <CR><LF> line endings to <LF>.
    * Fixed indention
    
    In check_reinvite_rtt.py:
    * Fixed type mismatch error when a slow 488 T.38 rejection is detected.
    * Fixed PEP8 errors
    
    Change-Id: Ic8e4d71f841627479c943c3b8a25e4776534258d

--- tests/fax/pjsip/t38_fast_reject/check_reinvite_rtt.py
+++ tests/fax/pjsip/t38_fast_reject/check_reinvite_rtt.py
@@ -8,16 +8,16 @@ regex = re.compile("([0-9]{2}):([0-9]{2}):([0-9]{2}):([0-9]{3})([0-9]{3})?")
 with open(sys.argv[1]) as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
     for row in reader:
-        if not row.has_key('ResponseTimereinvite(P)'):
+        if 'ResponseTimereinvite(P)' not in row:
             print "column not found! make sure scenario is correct!\n"
-            exit(-1);
+            exit(-1)
         parts = regex.match(row['ResponseTimereinvite(P)'])
         hours = int(parts.group(1))
         minutes = (hours * 60) + int(parts.group(2))
         seconds = (minutes * 60) + int(parts.group(3))
         milliseconds = (seconds * 1000) + int(parts.group(4))
         if (milliseconds > 500):
-            print "Slow 488 Rejection detected (" + milliseconds + ")!\n";
+            print "Slow 488 Rejection detected (" + str(milliseconds) + " ms)!\n"
             exit(-2)
 
 csvfile.close()
