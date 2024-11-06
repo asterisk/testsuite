b389570753913047c4d125707021e597649616e8
George Joseph <g.devel@wxy78.net>
2024-11-06 11:54:31 -0700

    .github: Reusable workflows
    Similar to what's already been done for Asterisk, the existing
    github workflows have been replaced with calls to reusable
    workflows from asterisk-ci-actions.
    
    Also refactored the self test slightly to not require an
    Asterisk build and install.  All it took was adding a recent
    asterisk buildopts.h to lib/python/asterisk/self_test.

---
+++ lib/python/asterisk/self_test/buildopts.h
@@ -0,0 +1,13 @@
+/*
+ * buildopts.h
+ * Automatically generated
+ */
+
+#define AST_DEVMODE 1
+#define DONT_OPTIMIZE 1
+#define OPTIONAL_API 1
+#define DO_CRASH 1
+#define TEST_FRAMEWORK 1
+#define AST_BUILDOPT_SUM "c1ede5e75208a56856a96370ccdead31"
+#define AST_BUILDOPTS "OPTIONAL_API, DO_CRASH, TEST_FRAMEWORK"
+#define AST_BUILDOPTS_ALL "DONT_OPTIMIZE, OPTIONAL_API, DO_CRASH, TEST_FRAMEWORK"
